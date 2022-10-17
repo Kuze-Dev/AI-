2954aad3187ca27532f6aa3418f62339d137313b
Julio Motol <jrmotol.halcyondigital@gmail.com>
2022-10-17 10:27:37 +0800

    🔥 Remove session check on `ConfirmPasswordAction`
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- tests/Unit/Domain/Auth/Actions/CofirmPasswordActionTest.php
+++ tests/Unit/Domain/Auth/Actions/CofirmPasswordActionTest.php
@@ -8,9 +8,6 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 it('can confirm password', function () {
-    Session::shouldReceive('isStarted')
-        ->once()
-        ->andReturn(true);
     Session::shouldReceive('put')
         ->once()
         ->andReturn();
@@ -23,21 +20,7 @@ it('can confirm password', function () {
     expect($result)->toBeTrue();
 });
 
-it('can\'t confirm password when session is not started', function () {
-    Session::shouldReceive('isStarted')
-        ->once()
-        ->andReturn(false);
-
-    $result = app(ConfirmPasswordAction::class)->execute('secret');
-
-    expect($result)->toBeFalse();
-});
-
 it('throws on invalid password', function () {
-    Session::shouldReceive('isStarted')
-        ->once()
-        ->andReturn(true);
-
     $validationException = ValidationException::withMessages([
         'password' => 'invalid',
     ]);
