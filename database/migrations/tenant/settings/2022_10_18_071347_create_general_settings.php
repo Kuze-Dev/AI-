334c05cd32e3934dacd27059885e13a2254983ab
Julio Motol <julio.motol89@gmail.com>
2022-10-31 11:56:32 +0800

    🐛 Fix site settings migrations for tenants
    Signed-off-by: Julio Motol <julio.motol89@gmail.com>

--- database/migrations/tenant/settings/2022_10_18_071347_create_general_settings.php
+++ database/migrations/tenant/settings/2022_10_18_071347_create_general_settings.php
@@ -7,10 +7,10 @@ use Spatie\LaravelSettings\Migrations\SettingsMigration;
 return new class () extends SettingsMigration {
     public function up(): void
     {
-        $this->migrator->add('site.site_name', 'Tenant Application');
-        $this->migrator->add('site.site_description', 'Welcome to your application!');
-        $this->migrator->add('site.site_author', 'Tenant');
-        $this->migrator->add('site.site_logo', '');
-        $this->migrator->add('site.site_favicon', '');
+        $this->migrator->add('site.name', 'Tenant Application');
+        $this->migrator->add('site.description', 'Welcome to your application!');
+        $this->migrator->add('site.author', 'Tenant');
+        $this->migrator->add('site.logo', '');
+        $this->migrator->add('site.favicon', '');
     }
 };
