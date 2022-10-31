d31bece435a92fc37657eacc1b5ad386613f7391
Julio Motol <julio.motol89@gmail.com>
2022-10-31 11:26:13 +0800

    👽️ Sync FilamentTenant `SiteSettings`
    Signed-off-by: Julio Motol <julio.motol89@gmail.com>

--- app/FilamentTenant/Pages/Settings/SiteSettings.php
+++ app/FilamentTenant/Pages/Settings/SiteSettings.php
@@ -19,32 +19,27 @@ class SiteSettings extends BaseSiteSettings
     {
         return [
             Card::make([
-                TextInput::make('site_name')
-                    ->label('Site Name')
+                TextInput::make('name')
                     ->required()
                     ->maxLength(100)
                     ->columnSpan('full'),
-                Textarea::make('site_description')
-                    ->label('Description')
+                Textarea::make('description')
                     ->required()
                     ->maxLength(255)
                     ->columnSpan('full'),
-                TextInput::make('site_author')
-                    ->label('Author')
+                TextInput::make('author')
                     ->required()
                     ->maxLength(100)
                     ->columnSpan('full'),
-                FileUpload::make('site_logo')
-                    ->label('Logo')
+                FileUpload::make('logo')
                     ->image()
                     ->acceptedFileTypes(['image/png', 'image/webp', 'image/jpg', 'image/jpeg'])
                     ->maxSize(1000)
                     ->required()
                     ->getUploadedFileUrlUsing(fn (string $file) => tenant_asset($file)),
-                FileUpload::make('site_favicon')
+                FileUpload::make('favicon')
                     ->image()
                     ->acceptedFileTypes(['image/ico', 'image/png', 'image/webp', 'image/jpg', 'image/jpeg'])
-                    ->label('Favicon')
                     ->imageResizeTargetHeight('100')
                     ->imageResizeTargetWidth('100')
                     ->maxSize(1000)
