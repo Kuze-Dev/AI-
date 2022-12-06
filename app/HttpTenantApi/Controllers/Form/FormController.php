a77ac9f685558280caf4f00b9e0477b805770891
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2022-12-06 09:15:29 +0800

    Fix test, analyse
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- app/HttpTenantApi/Controllers/Form/FormController.php
+++ app/HttpTenantApi/Controllers/Form/FormController.php
@@ -8,17 +8,18 @@ use App\Http\Controllers\Controller;
 use App\HttpTenantApi\Resources\FormResource;
 use Domain\Form\Models\Form;
 use Spatie\RouteAttributes\Attributes\Get;
+use TiMacDonald\JsonApi\JsonApiResourceCollection;
 
 class FormController extends Controller
 {
     #[Get('forms')]
-    public function index()
+    public function index(): JsonApiResourceCollection
     {
         return FormResource::collection(Form::with('blueprint')->paginate());
     }
 
     #[Get('forms/{form}')]
-    public function show(Form $form)
+    public function show(Form $form): FormResource
     {
         return FormResource::make($form);
     }
