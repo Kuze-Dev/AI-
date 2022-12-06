dcc11e43347e5e165129a5448df46fa791aae32a
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2022-12-06 09:16:46 +0800

    Use ApiResource route attribute
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- app/HttpTenantApi/Controllers/Form/FormController.php
+++ app/HttpTenantApi/Controllers/Form/FormController.php
@@ -7,18 +7,17 @@ namespace App\HttpTenantApi\Controllers\Form;
 use App\Http\Controllers\Controller;
 use App\HttpTenantApi\Resources\FormResource;
 use Domain\Form\Models\Form;
-use Spatie\RouteAttributes\Attributes\Get;
+use Spatie\RouteAttributes\Attributes\ApiResource;
 use TiMacDonald\JsonApi\JsonApiResourceCollection;
 
+#[ApiResource('forms', only: ['index', 'show'])]
 class FormController extends Controller
 {
-    #[Get('forms')]
     public function index(): JsonApiResourceCollection
     {
         return FormResource::collection(Form::with('blueprint')->paginate());
     }
 
-    #[Get('forms/{form}')]
     public function show(Form $form): FormResource
     {
         return FormResource::make($form);
