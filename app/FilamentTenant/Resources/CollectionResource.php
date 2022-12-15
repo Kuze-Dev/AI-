1d99aee3f6a5c8aa3df55dcd8169ef105c627bd7
Julio Motol <jrmotol.halcyondigital@gmail.com>
2022-12-15 13:51:27 +0800

    🚚 Update `entry.create` path
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- app/FilamentTenant/Resources/CollectionResource.php
+++ app/FilamentTenant/Resources/CollectionResource.php
@@ -186,7 +186,7 @@ class CollectionResource extends Resource
             'index' => Resources\CollectionResource\Pages\ListCollection::route('/'),
             'create' => Resources\CollectionResource\Pages\CreateCollection::route('/create'),
             'configure' => Resources\CollectionResource\Pages\ConfigureCollection::route('/{record}/configure'),
-            'entry.create' => Resources\CollectionResource\Pages\CreateCollectionEntry::route('/{ownerRecord}/create'),
+            'entry.create' => Resources\CollectionResource\Pages\CreateCollectionEntry::route('/{ownerRecord}/entry/create'),
             'entry.edit' => Resources\CollectionResource\Pages\EditCollectionEntry::route('/{ownerRecord}/entry/{record}/edit'),
         ];
     }
