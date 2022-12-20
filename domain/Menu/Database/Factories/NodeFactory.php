b07ffbe743bbc4b6d683baf69a4d13aa49feb3c7
John Leward Escote <jlescote.halcyondigital@gmail.com>
2022-12-20 15:13:15 +0800

    fix: composer analyse and format
    Signed-off-by: John Leward Escote <jlescote.halcyondigital@gmail.com>

---
+++ domain/Menu/Database/Factories/NodeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Menu\Database\Factories;
+
+use Domain\Menu\Models\Node;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Menu\Models\Node>
+ */
+class NodeFactory extends Factory
+{
+    protected $model = Node::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'target' => '_blank',
+            'url' => fake()->url(),
+        ];
+    }
+}
