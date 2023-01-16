9abb70803c1be9e80a7aebb2fe3bf2e6798b96c2
Julio Motol <jrmotol.halcyondigital@gmail.com>
2023-01-16 18:28:36 +0800

    ♻️ Update DTO typings
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- domain/Collection/DataTransferObjects/CollectionData.php
+++ domain/Collection/DataTransferObjects/CollectionData.php
@@ -10,12 +10,12 @@ class CollectionData
 {
     public function __construct(
         public readonly string $name,
-        public readonly ?string $blueprint_id = null,
+        public readonly string $blueprint_id,
         public readonly array $taxonomies = [],
         public readonly ?string $slug = null,
         public readonly ?PublishBehavior $past_publish_date_behavior = null,
         public readonly ?PublishBehavior $future_publish_date_behavior = null,
-        public readonly ?bool $is_sortable = false,
+        public readonly bool $is_sortable = false,
     ) {
     }
 }
--- domain/Collection/DataTransferObjects/CollectionEntryData.php
+++ domain/Collection/DataTransferObjects/CollectionEntryData.php
@@ -10,10 +10,10 @@ class CollectionEntryData
 {
     public function __construct(
         public readonly string $title,
-        public readonly ?string $slug,
+        public readonly ?string $slug = null,
         public readonly array $taxonomy_terms = [],
         public readonly ?Carbon $published_at = null,
-        public readonly ?array $data = [],
+        public readonly array $data = [],
     ) {
     }
 }
