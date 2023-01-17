56555dda0c6920b75c8fb8228b2dc3c5db0a5bf9
Roy <rbiscocho.halcyondigital@gmail.com>
2023-01-17 14:46:10 +0800

    add blueprint global search test
    Signed-off-by: Roy <rbiscocho.halcyondigital@gmail.com>

---
+++ tests/Feature/FilamentTenant/Resources/BlueprintResource/BlueprintGlobalSearchTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Filament\Facades\Filament;
+use Domain\Form\Database\Factories\FormFactory;
+
+beforeEach(function () {
+    testInTenantContext();
+    Filament::setContext('filament-tenant');
+    loginAsSuperAdmin();
+});
+
+it('blueprint must have a getGloballySearchableAttribute', function () {
+
+    $data = FormFactory::new()
+            ->withDummyBlueprint()
+            ->create();
+    
+    $results = Filament::getGlobalSearchProvider()
+            ->getResults($data->name);
+
+    $this->assertEquals(
+        route('filament-tenant.resources.forms.edit',
+        $data->getRouteKey()
+    ),
+
+        $results->getCategories()['forms']->first()->url
+        
+    );
+   
+});
+
