d11d89060531a8992363d0dca5f56dbab08b0ed8
Roy <rbiscocho.halcyondigital@gmail.com>
2023-01-24 08:54:46 +0800

    updating resolveRouteBindingQuery Annotation
    Signed-off-by: Roy <rbiscocho.halcyondigital@gmail.com>

--- domain/Page/Actions/CreatePageAction.php
+++ domain/Page/Actions/CreatePageAction.php
@@ -6,7 +6,6 @@ namespace Domain\Page\Actions;
 
 use Domain\Page\DataTransferObjects\PageData;
 use Domain\Page\Models\Page;
-use Domain\Page\Models\RecordsSlugHistory;
 
 class CreatePageAction
 {
@@ -22,16 +21,6 @@ class CreatePageAction
             'slug' => $pageData->slug,
         ]);
 
-        // $slug = RecordsSlugHistory::where('slug', $page->slug)
-        //     ->where('sluggable_type', $page->getMorphClass())->first();
-
-        // if ( ! empty($slug)) {
-        //     $slug->sluggable_id = $page->id;
-        //     $slug->save();
-        // } else {
-        //     $page->sluggable()->updateorcreate(['slug' => $pageData->slug]);
-        // }
-        
         foreach ($pageData->slice_contents as $sliceContentData) {
             $this->createSliceContent->execute($page, $sliceContentData);
         }
--- domain/Page/Models/Traits/HasSlugHistoryTrait.php
+++ domain/Page/Models/Traits/HasSlugHistoryTrait.php
@@ -9,46 +9,40 @@ use Domain\Page\Models\RecordsSlugHistory;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\Builder;
 
+/**  
+ * @template TRelatedModel of RecordsSlugHistory
+ * 
+ */
 trait HasSlugHistoryTrait
 {
-
-    public static function boot()
+    public static function boot(): void
     {
-
-        if(!static::getEventDispatcher()){
-            static::setEventDispatcher( new \Illuminate\Events\Dispatcher());
-        }
-
         parent::boot();
 
-        static::created( function($model) {
-
+        static::created(function ($model) {
             $slug = RecordsSlugHistory::where('slug', $model->slug)
-            ->where('sluggable_type', $model->getMorphClass())->first();
+                ->where('sluggable_type', $model->getMorphClass())->first();
 
-        if ( ! empty($slug)) {
-            $slug->sluggable_id = $model->id;
-            $slug->save();
-        } else {
-            $model->sluggable()->updateorcreate(['slug' => $model->slug]);
-        }
-        
+            if ( ! empty($slug)) {
+                $slug->sluggable_id = $model->id;
+                $slug->save();
+            } else {
+                $model->sluggable()->updateorcreate(['slug' => $model->slug]);
+            }
         });
 
-
-        static::updated(function($model) {
-
+        static::updated(function ($model) {
             $slug = RecordsSlugHistory::where('slug', $model->slug)
-            ->where('sluggable_type', $model->getMorphClass())->first();
+                ->where('sluggable_type', $model->getMorphClass())->first();
 
-        if ( ! empty($slug)) {
-            $slug->sluggable_id = $model->id;
-            $slug->save();
-        } else {
-            $model->sluggable()->updateorcreate(['slug' => $model->slug]);
-        }
-
+            if ( ! empty($slug)) {
+                $slug->sluggable_id = $model->id;
+                $slug->save();
+            } else {
+                $model->sluggable()->updateorcreate(['slug' => $model->slug]);
+            }
         });
     }
 
@@ -59,7 +53,12 @@ trait HasSlugHistoryTrait
         return $this->morphMany(RecordsSlugHistory::class, 'sluggable');
     }
 
-    public function resolveRouteBindingQuery($query, $value, $field = null)
+   /**
+    * @param static|Relation<static> $query
+    *
+    * @return Relation|Builder<static> 
+    */
+    public function resolveRouteBindingQuery( $query, $value, $field = null) : Builder
     {
         return $query->where($field ?? $this->getRouteKeyName(), $value)
             ->orWhereHas('sluggable', fn ($q) => $q->where('slug', $value));
