d6229233fe71c66a772f3e3e57746ee7fcb7dd01
Julio Motol <jrmotol.halcyondigital@gmail.com>
2023-02-09 14:37:17 +0800

    ✨ Should only be collapsed on edit form
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- app/FilamentTenant/Resources/PageResource.php
+++ app/FilamentTenant/Resources/PageResource.php
@@ -82,8 +82,7 @@ class PageResource extends Resource
                         ->itemLabel(fn (array $state) => self::getCachedSlices()->firstWhere('id', $state['slice_id'])?->name)
                         ->disableLabel()
                         ->minItems(1)
-                        ->collapsed()
-                        ->collapsible()
+                        ->collapsed(fn (string $context) => $context === 'edit')
                         ->orderable('order')
                         ->schema([
                             Forms\Components\Select::make('slice_id')
