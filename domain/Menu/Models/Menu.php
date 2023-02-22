7d1db8d53b95394461b39e5e147784bf9aa8dc41
Julio Motol <jrmotol.halcyondigital@gmail.com>
2023-02-22 10:21:45 +0800

    📝 Fix `Menu` model docblock
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- domain/Menu/Models/Menu.php
+++ domain/Menu/Models/Menu.php
@@ -22,7 +22,7 @@ use Spatie\Sluggable\SlugOptions;
  * @property string $name
  * @property string $slug
  * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\SupparentNodesport\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|Activity[] $activities
  * @property-read int|null $activities_count
  * @property-read \Illuminate\Database\Eloquent\Collection|\Domain\Menu\Models\Node[] $nodes
