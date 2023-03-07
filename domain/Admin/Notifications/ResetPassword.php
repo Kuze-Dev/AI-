aeffcd0bfe08f9ce1a5e85461cc3c92cbb270b5c
Roy <rbiscocho.halcyondigital@gmail.com>
2023-03-07 14:41:18 +0800

    fix email verificationa and password reset link redirection
    Signed-off-by: Roy <rbiscocho.halcyondigital@gmail.com>

--- domain/Admin/Notifications/ResetPassword.php
+++ domain/Admin/Notifications/ResetPassword.php
@@ -10,4 +10,18 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class ResetPassword extends \Illuminate\Auth\Notifications\ResetPassword implements ShouldQueue
 {
     use Queueable;
+
+    /**
+     * Get the reset URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function resetUrl($notifiable)
+    {
+        return url(route('filament-tenant.auth.password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+    }
 }
--- domain/Admin/Notifications/VerifyEmail.php
+++ domain/Admin/Notifications/VerifyEmail.php
@@ -6,8 +6,30 @@ namespace Domain\Admin\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Auth\Notifications\VerifyEmail as BaseVerifyMail;
 
-class VerifyEmail extends \Illuminate\Auth\Notifications\VerifyEmail implements ShouldQueue
+class VerifyEmail extends BaseVerifyMail implements ShouldQueue
 {
     use Queueable;
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        return URL::temporarySignedRoute(
+            'filament-tenant.auth.verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
+    }
 }
