725a4c95c88e093dc920b406db25731e7e33ce66
Roy <rbiscocho.halcyondigital@gmail.com>
2023-03-08 09:26:58 +0800

    admin tenantBaseSettings Class
    Signed-off-by: Roy <rbiscocho.halcyondigital@gmail.com>

--- app/FilamentTenant/Pages/Settings/SiteSettings.php
+++ app/FilamentTenant/Pages/Settings/SiteSettings.php
@@ -4,20 +4,53 @@ declare(strict_types=1);
 
 namespace App\FilamentTenant\Pages\Settings;
 
-use App\Filament\Pages\Settings\SiteSettings as BaseSiteSettings;
-use App\FilamentTenant\Pages\Settings\Concerns\ContextualSettingsPage;
+use App\Settings\SiteSettings as ManageSiteSettings;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Livewire\TemporaryUploadedFile;
 
-class SiteSettings extends BaseSiteSettings
+class SiteSettings extends TenantBaseSettings
 {
-    use ContextualSettingsPage;
+    protected static string $settings = ManageSiteSettings::class;
 
-    protected function getBreadcrumbs(): array
+    protected static ?string $navigationIcon = 'heroicon-o-globe-alt';
+
+    protected function getFormSchema(): array
     {
-        $breadcrumb = $this->getBreadcrumb();
-
-        return array_merge(
-            [route('filament-tenant.pages.settings') => trans('Settings')],
-            (filled($breadcrumb) ? [$breadcrumb] : [])
-        );
+        return [
+            Card::make([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(100)
+                    ->columnSpan('full'),
+                Textarea::make('description')
+                    ->required()
+                    ->maxLength(255)
+                    ->columnSpan('full'),
+                TextInput::make('author')
+                    ->required()
+                    ->maxLength(100)
+                    ->columnSpan('full'),
+                FileUpload::make('logo')
+                    ->acceptedFileTypes(['image/png', 'image/webp', 'image/jpg', 'image/jpeg'])
+                    ->maxSize(1_000)
+                    ->required()
+                    ->getUploadedFileNameForStorageUsing(static function (TemporaryUploadedFile $file) {
+                        return 'logo.'.$file->extension();
+                    }),
+                FileUpload::make('favicon')
+                    ->acceptedFileTypes(['image/ico', 'image/png', 'image/webp', 'image/jpg', 'image/jpeg'])
+                    ->imageResizeTargetHeight('100')
+                    ->imageResizeTargetWidth('100')
+                    ->maxSize(1_000)
+                    ->required()
+                    ->getUploadedFileNameForStorageUsing(static function (TemporaryUploadedFile $file) {
+                        return 'favicon.'.$file->extension();
+                    }),
+            ])
+                ->columns(2),
+        ];
     }
 }
