8dbda36e28649300a4aad9a7b13d9d553e43a243
Julio Motol <jrmotol.halcyondigital@gmail.com>
2023-03-14 16:24:28 +0800

    🚸 Elaborate confirmation modal subheading
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -11,6 +11,7 @@ use App\Filament\Livewire\Auth\RequestPasswordReset;
 use App\Filament\Livewire\Auth\ResetPassword;
 use App\Filament\Livewire\Auth\TwoFactorAuthentication;
 use App\Filament\Livewire\Auth\VerifyEmail;
+use Closure;
 use Domain\Admin\Models\Admin;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationGroup;
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Saade\FilamentLaravelLog\Pages\ViewLog;
+use Filament\Pages\Actions as PageActions;
+use Filament\Tables\Actions as TableActions;
 
 /** @property \Illuminate\Foundation\Application $app */
 class FilamentServiceProvider extends ServiceProvider
@@ -68,6 +71,8 @@ class FilamentServiceProvider extends ServiceProvider
         ViewLog::can(fn (?Admin $admin) => $admin?->hasRole(config('domain.role.super_admin')));
 
         $this->registerRoutes();
+
+        $this->configureComponents();
     }
 
     protected function registerRoutes(): void
@@ -110,5 +115,43 @@ class FilamentServiceProvider extends ServiceProvider
                             });
                     });
             });
+    }
+
+    protected function configureComponents(): void
+    {
+        PageActions\DeleteAction::configureUsing($this->createModalSubheadingConfiguration('delete'), isImportant: true);
+        PageActions\RestoreAction::configureUsing($this->createModalSubheadingConfiguration('restore'), isImportant: true);
+        PageActions\ForceDeleteAction::configureUsing($this->createModalSubheadingConfiguration('force delete'), isImportant: true);
+
+        TableActions\DeleteAction::configureUsing($this->createModalSubheadingConfiguration('delete'), isImportant: true);
+        TableActions\RestoreAction::configureUsing($this->createModalSubheadingConfiguration('restore'), isImportant: true);
+        TableActions\ForceDeleteAction::configureUsing($this->createModalSubheadingConfiguration('force delete'), isImportant: true);
+
+        TableActions\DeleteBulkAction::configureUsing($this->createBulkModalSubheadingConfiguration('delete'), isImportant: true);
+        TableActions\RestoreBulkAction::configureUsing($this->createBulkModalSubheadingConfiguration('restore'), isImportant: true);
+        TableActions\ForceDeleteBulkAction::configureUsing($this->createBulkModalSubheadingConfiguration('force delete'), isImportant: true);
+    }
+
+    private function createModalSubheadingConfiguration(string $verb): Closure
+    {
+        return fn (PageActions\Action|TableActions\Action $action) => $action->modalSubheading(
+            fn (PageActions\Action|TableActions\Action $action) => trans(
+                "Are you sure you want to {$verb} this :resource?",
+                ['resource' => $action->getModelLabel() ?? 'record']
+            )
+        );
+    }
+
+    private function createBulkModalSubheadingConfiguration(string $verb): Closure
+    {
+        return fn (TableActions\BulkAction $action) => $action->modalSubheading(
+            fn (TableActions\BulkAction $action) => trans(
+                "Are you sure you want to {$verb} :count :resource?",
+                [
+                    'resource' => $action->getPluralModelLabel(),
+                    'count' => $action->getRecords()?->count() ?? 0
+                ]
+            )
+        );
     }
 }
