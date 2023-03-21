894be5498108068281be45e387728ddbfe69cc91
Roy <rbiscocho.halcyondigital@gmail.com>
2023-03-21 08:28:23 +0800

    The maxLength method is resulting in a failed test when a value is passed, but if a closure is passed, it passes successfully
    Signed-off-by: Roy <rbiscocho.halcyondigital@gmail.com>

--- app/FilamentTenant/Support/MetaDataForm.php
+++ app/FilamentTenant/Support/MetaDataForm.php
@@ -25,7 +25,7 @@ class MetaDataForm extends Section
             Forms\Components\TextInput::make('author')
                 ->formatStateUsing(fn ($record) => $record?->metaData?->author),
             Forms\Components\Textarea::make('description')
-                ->maxLength(160)
+                ->maxLength(fn () => 160)
                 ->formatStateUsing(fn ($record) => $record?->metaData?->description),
             Forms\Components\FileUpload::make('image')
                 ->formatStateUsing(function ($record) {
