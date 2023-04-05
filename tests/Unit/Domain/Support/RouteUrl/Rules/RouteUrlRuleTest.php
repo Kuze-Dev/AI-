c159ca98bec7f22e9a3b413751dfe1f996163258
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-04-05 15:44:39 +0800

    Add more test for validation rule
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- tests/Unit/Domain/Support/RouteUrl/Rules/RouteUrlRuleTest.php
+++ tests/Unit/Domain/Support/RouteUrl/Rules/RouteUrlRuleTest.php
@@ -10,10 +10,13 @@ use Domain\Support\RouteUrl\Rules\RouteUrlRule;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Validation\InvokableValidationRule;
 use Tests\Fixtures\TestModelForRouteUrl;
+use Illuminate\Validation\Validator;
+use Illuminate\Translation\ArrayLoader;
+use Illuminate\Translation\Translator;
 
 use function Pest\Laravel\assertDatabaseEmpty;
+use function PHPUnit\Framework\assertFalse;
 use function PHPUnit\Framework\assertTrue;
 
 uses()->group('route_url');
@@ -35,12 +38,11 @@ beforeEach(function () {
 
 it('passed w/o any', function () {
     assertTrue(
-        routeUrlRule(TestModelForRouteUrl::class, null)
-            ->passes('attribute', 'test')
+        routeUrlRule(TestModelForRouteUrl::class, null, 'test')
     );
 });
 
-it('edit current data w/o modifying is passed', function () {
+it('can edit current data w/o modifying is passed', function () {
     $model = TestModelForRouteUrl::create([
         'name' => 'my-awesome-name',
     ]);
@@ -49,12 +51,99 @@ it('edit current data w/o modifying is passed', function () {
         ->execute($model, new RouteUrlData(null));
 
     assertTrue(
-        routeUrlRule($model::class, $model)
-            ->passes('attribute', $model->getActiveRouteUrl()->url)
+        routeUrlRule($model::class, $model, $model->getActiveRouteUrl()->url)
     );
 });
 
-function routeUrlRule(string $modelUsed, ?HasRouteUrl $model): InvokableValidationRule
+it('passed when using old history of ur on create', function () {
+    $model = TestModelForRouteUrl::create([
+        'name' => 'my-awesome-name',
+    ]);
+
+    app(CreateOrUpdateRouteUrlAction::class)
+        ->execute($model, new RouteUrlData('old-one'));
+
+    app(CreateOrUpdateRouteUrlAction::class)
+        ->execute($model, new RouteUrlData('one'));
+
+    assertTrue(
+        routeUrlRule($model::class, null, 'old-one')
+    );
+});
+
+it('passed when using old history of ur on update', function () {
+    $model = TestModelForRouteUrl::create([
+        'name' => 'my-awesome-name',
+    ]);
+
+    app(CreateOrUpdateRouteUrlAction::class)
+        ->execute($model, new RouteUrlData('old-one'));
+
+    app(CreateOrUpdateRouteUrlAction::class)
+        ->execute($model, new RouteUrlData('one'));
+
+    assertTrue(
+        routeUrlRule($model::class, $model, 'old-one')
+    );
+});
+
+it('fail on duplicate active url on create', function (bool $hasHistory) {
+    $model = TestModelForRouteUrl::create([
+        'name' => 'my-awesome-name',
+    ]);
+
+    if ($hasHistory) {
+        app(CreateOrUpdateRouteUrlAction::class)
+            ->execute($model, new RouteUrlData('old-one'));
+    }
+
+    app(CreateOrUpdateRouteUrlAction::class)
+        ->execute($model, new RouteUrlData('one'));
+
+    assertFalse(
+        routeUrlRule($model::class, null, 'one')
+    );
+})
+    ->with(['has History' => true, 'has no history' => false]);
+
+it('fail on duplicate active url on update', function (bool $hasHistory) {
+    $model = TestModelForRouteUrl::create([
+        'name' => 'my-awesome-name',
+    ]);
+    if ($hasHistory) {
+        app(CreateOrUpdateRouteUrlAction::class)
+            ->execute($model, new RouteUrlData('old-one'));
+    }
+
+    app(CreateOrUpdateRouteUrlAction::class)
+        ->execute($model, new RouteUrlData('one'));
+
+    $model2 = TestModelForRouteUrl::create([
+        'name' => 'my-awesome-name',
+    ]);
+
+    app(CreateOrUpdateRouteUrlAction::class)
+        ->execute($model2, new RouteUrlData('two'));
+
+    assertFalse(
+        routeUrlRule($model::class, $model2, 'one')
+    );
+})
+    ->with(['has History' => true, 'has no history' => false]);
+
+/**
+ *  if $model is null, it means it uses on create, else on edit
+ */
+function routeUrlRule(string $modelUsed, ?HasRouteUrl $model, string $dataToBeValidate): bool
 {
-    return InvokableValidationRule::make(new RouteUrlRule($modelUsed, $model));
+    $r = new Validator(
+        new Translator(
+            new ArrayLoader(),
+            'en'
+        ),
+        ['data' => $dataToBeValidate],
+        ['data' => new RouteUrlRule($modelUsed, $model)]
+    );
+
+    return $r->passes();
 }
