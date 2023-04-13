e32ac0cb7925140892b5dc9775eee739783e418a
Julio Motol <jrmotol.halcyondigital@gmail.com>
2023-04-13 17:03:36 +0800

    🔥 Remove unecessary rebinding of `SettingsEventSubscriber`
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -4,21 +4,15 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Settings\Subscriber\SettingsEventSubscriber as SettingsEventSubscriberApp;
 use App\Settings\Support\SettingsCacheFactory as SettingsCacheFactoryApp;
 use Illuminate\Support\ServiceProvider;
 use Spatie\LaravelSettings\SettingsContainer;
-use Spatie\LaravelSettings\SettingsEventSubscriber as SettingsEventSubscriberSpatie;
 use Spatie\LaravelSettings\Support\SettingsCacheFactory as SettingsCacheFactorySpatie;
 
 class SettingServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(
-            SettingsEventSubscriberSpatie::class,
-            SettingsEventSubscriberApp::class
-        );
         $this->app->bind(
             SettingsCacheFactorySpatie::class,
             fn () => new SettingsCacheFactoryApp()
