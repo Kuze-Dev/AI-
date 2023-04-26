9e41b98717a8801ada802929bb9f1abb6b50d5f2
Julio Motol <jrmotol.halcyondigital@gmail.com>
2023-04-26 09:08:23 +0800

    🐛 DB Driver aware migration
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- database/migrations/tenant/2023_04_24_044224_add_prefix_column_on_contents_table.php
+++ database/migrations/tenant/2023_04_24_044224_add_prefix_column_on_contents_table.php
@@ -6,18 +6,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /** Run the migrations. */
     public function up(): void
     {
-        Schema::table('contents', function (Blueprint $table) {
-            $table->string('prefix')->nullable()->unique();
-        });
+        if (DB::getDriverName() === 'sqlite') {
+            Schema::table('contents', function (Blueprint $table) {
+                $table->string('prefix')->nullable()->unique();
+            });
 
-        // WORKAROUND: sqlite not allowing to add a NOT NULL column. https://stackoverflow.com/q/3170634
-        Schema::table('contents', function (Blueprint $table) {
-            $table->string('prefix')->nullable(false)->change();
-        });
+            // WORKAROUND: sqlite not allowing to add a NOT NULL column. https://stackoverflow.com/q/3170634
+            Schema::table('contents', function (Blueprint $table) {
+                $table->string('prefix')->nullable(false)->change();
+            });
+        } else {
+            Schema::table('contents', function (Blueprint $table) {
+                $table->string('prefix')->unique();
+            });
+        }
     }
 
     /** Reverse the migrations. */
