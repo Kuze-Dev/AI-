02681a5fa0438426ecebab19fd3b83836b50b93a
Julio Motol <jrmotol.halcyondigital@gmail.com>
2023-04-26 10:10:32 +0800

    🗃️ Assign default value for content prefix before assigning index
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- database/migrations/tenant/2023_04_24_044224_add_prefix_column_on_contents_table.php
+++ database/migrations/tenant/2023_04_24_044224_add_prefix_column_on_contents_table.php
@@ -2,18 +2,19 @@
 
 declare(strict_types=1);
 
+use Domain\Content\Models\Content;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
-{
+return new class () extends Migration {
     /** Run the migrations. */
     public function up(): void
     {
         if (DB::getDriverName() === 'sqlite') {
             Schema::table('contents', function (Blueprint $table) {
-                $table->string('prefix')->nullable()->unique();
+                $table->string('prefix')->nullable()->after('slug');
             });
 
             // WORKAROUND: sqlite not allowing to add a NOT NULL column. https://stackoverflow.com/q/3170634
@@ -22,9 +23,22 @@ return new class() extends Migration
             });
         } else {
             Schema::table('contents', function (Blueprint $table) {
-                $table->string('prefix')->unique();
+                $table->string('prefix')->after('slug');
             });
         }
+
+        DB::table((new Content())->getTable())
+            ->orderBy('id')
+            ->lazy()
+            ->each(
+                fn ($row) => DB::table((new Content())->getTable())
+                    ->where('id', $row->id)
+                    ->update(['prefix' => $row->slug])
+            );
+
+        Schema::table('contents', function (Blueprint $table) {
+            $table->unique('prefix');
+        });
     }
 
     /** Reverse the migrations. */
