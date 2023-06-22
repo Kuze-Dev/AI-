eaa2d5e394a57aa7c838427e7408845d7b24efe9
Roy Biscocho <rbiscocho.halcyondigital@gmail.com>
2023-06-22 09:28:22 +0800

    add docblock
    Signed-off-by: Roy Biscocho <rbiscocho.halcyondigital@gmail.com>

--- domain/PaymentMethod/Models/PaymentMethod.php
+++ domain/PaymentMethod/Models/PaymentMethod.php
@@ -14,6 +14,43 @@ use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
+/**
+ * Domain\PaymentMethod\Models\PaymentMethod
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $slug
+ * @property string $gateway
+ * @property string|null $subtitle
+ * @property bool $status
+ * @property string|null $description
+ * @property array|null $credentials
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Activitylog\Models\Activity> $activities
+ * @property-read int|null $activities_count
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property-read int|null $media_count
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereCredentials($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereGateway($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereSubtitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod withTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod withoutTrashed()
+ * @mixin \Eloquent
+ */
 class PaymentMethod extends Model implements HasMedia
 {
     use HasFactory;
