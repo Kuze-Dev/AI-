72a6e15bc02971274c420b9e31cd9c8b6e97dff4
Marlon Nino <mvnino.halcyondigital@gmail.com>
2023-06-26 16:56:30 +0800

    feat: wip for product resource variant modal

--- app/FilamentTenant/Resources/ProductResource.php
+++ app/FilamentTenant/Resources/ProductResource.php
@@ -46,49 +46,21 @@ class ProductResource extends Resource
                             ->label('Product Name')
                             ->unique(ignoreRecord: true)
                             ->required(),
-                        Forms\Components\Group::make()
-                            ->schema([
-                                Forms\Components\Group::make()
-                                    ->statePath('taxonomies')
-                                    ->schema(
-                                        fn () => $taxonomies->map(
-                                            fn (Taxonomy $taxonomy) => Forms\Components\Select::make($taxonomy->name)
-                                                ->statePath((string) $taxonomy->id)
-                                                ->multiple(
-                                                    fn () => $taxonomy->slug === 'brand' ? false : true
-                                                )
-                                                ->options(
-                                                    $taxonomy->taxonomyTerms->sortBy('name')
-                                                        ->mapWithKeys(fn (TaxonomyTerm $term) => [$term->id => $term->name])
-                                                        ->toArray()
-                                                )
-                                                ->formatStateUsing(
-                                                    fn (?Product $record) => $record?->taxonomyTerms->where('taxonomy_id', $taxonomy->id)
-                                                        ->pluck('id')
-                                                        ->toArray() ?? []
-                                                )
-                                                ->required()
-                                        )->toArray()
-                                    )
-                                    ->dehydrated(false),
-                                Forms\Components\Hidden::make('taxonomy_terms')
-                                    ->dehydrateStateUsing(fn (Closure $get) => Arr::flatten($get('taxonomies') ?? [], 1)),
-                            ])
-                            ->when(fn () => ! empty($taxonomies->toArray())),
                         Forms\Components\RichEditor::make('description')->required(),
                         Forms\Components\FileUpload::make('image')
                             ->label('Media')
                             ->mediaLibraryCollection('image')
                             ->image()
-                            ->required(),
-                        Forms\Components\Section::make('Customer Remarks')
-                            ->schema([
-                                Forms\Components\Toggle::make('allow_customer_remarks')
-                                    ->label('Allow customer to add remarks upon purchase'),
-                                Forms\Components\Checkbox::make('allow_remark_with_image')
-                                    ->label('Allow to add media'),
-                            ]),
+                            ->required()
                     ]),
+
+                    Forms\Components\Section::make('Customer Remarks')
+                        ->schema([
+                            Forms\Components\Toggle::make('allow_customer_remarks')
+                                ->label('Allow customer to add remarks upon purchase'),
+                            Forms\Components\Checkbox::make('allow_remark_with_image')
+                                ->label('Allow to add media'),
+                        ]),
                     Forms\Components\Section::make('Section Display')
                         ->schema([
                             Forms\Components\Toggle::make('is_special_offer'),
@@ -106,9 +78,9 @@ class ProductResource extends Resource
                                 ->dehydrateStateUsing(fn ($state) => (float) $state)
                                 ->helperText('Leave this field blank if there is no shipping fee.'),
                         ]),
-                    // Reference: Block & Taxonomy
                     Forms\Components\Section::make(trans('Variants (work in progress)'))->schema([
-                        Tree::make('variants')
+                        // For Manage Variant
+                        Tree::make('manage_variants')
                             ->formatStateUsing(
                                 fn () => []
                             )
@@ -120,11 +92,32 @@ class ProductResource extends Resource
                                             ->required()
                                             ->unique(ignoreRecord: true),
                                     ]),
-                                Forms\Components\FileUpload::make('image')
-                                    ->label('Product Image')
-                                    // ->mediaLibraryCollection('image')
-                                    ->image()
-                                    ->required(),
+                                Forms\Components\Grid::make(['md' => 1])
+                                    ->schema([
+                                        Forms\Components\TextInput::make('variant_values')
+                                            ->required()
+                                            ->unique(ignoreRecord: true),
+                                    ]),
+                            ])->hiddenOn('create'),
+
+
+
+                        // Reference: Block & Taxonomy
+                        Tree::make('variants')
+                            ->formatStateUsing(
+                                fn () => []
+                            )
+                            ->itemLabel(fn (array $state) => $state['name'] ?? null)
+                            ->schema([
+                                Forms\Components\Group::make()
+                                    ->schema([
+                                        Forms\Components\TextInput::make('size')
+                                            ->required()
+                                            ->unique(ignoreRecord: true),
+                                        Forms\Components\TextInput::make('color')
+                                            ->required()
+                                            ->unique(ignoreRecord: true),
+                                    ])->columns(2),
                                 Forms\Components\Section::make('Inventory & Shipping')
                                     ->schema([
                                         Forms\Components\TextInput::make('sku')
@@ -164,18 +157,6 @@ class ProductResource extends Resource
                                             ->helperText('This product will be hidden from all sales channels.'),
 
                                     ])->columns(2),
-                                // Reference: TenantResource
-                                Forms\Components\Section::make('Dynamic Attributes')
-                                    ->schema([
-                                        Forms\Components\Repeater::make('dynamic_attributes')
-                                            ->schema([
-                                                Forms\Components\TextInput::make('attribute_name')->required(),
-                                                Forms\Components\TextInput::make('attribute_value')->required(),
-                                            ])
-                                            ->disableItemMovement()
-                                            ->defaultItems(1)
-                                            ->columns(2),
-                                    ]),
                             ])->hiddenOn('create'),
                     ]),
                     Forms\Components\Section::make('Inventory')
@@ -221,6 +202,38 @@ class ProductResource extends Resource
                                 )
                                 ->helperText('This product will be hidden from all sales channels.'),
                         ]),
+                    Forms\Components\Section::make('Associations')
+                        ->schema([
+                            Forms\Components\Group::make()
+                                ->schema([
+                                    Forms\Components\Group::make()
+                                        ->statePath('taxonomies')
+                                        ->schema(
+                                            fn () => $taxonomies->map(
+                                                fn (Taxonomy $taxonomy) => Forms\Components\Select::make($taxonomy->name)
+                                                    ->statePath((string) $taxonomy->id)
+                                                    ->multiple(
+                                                        fn () => $taxonomy->slug === 'brand' ? false : true
+                                                    )
+                                                    ->options(
+                                                        $taxonomy->taxonomyTerms->sortBy('name')
+                                                            ->mapWithKeys(fn (TaxonomyTerm $term) => [$term->id => $term->name])
+                                                            ->toArray()
+                                                    )
+                                                    ->formatStateUsing(
+                                                        fn (?Product $record) => $record?->taxonomyTerms->where('taxonomy_id', $taxonomy->id)
+                                                            ->pluck('id')
+                                                            ->toArray() ?? []
+                                                    )
+                                                    ->required()
+                                            )->toArray()
+                                        )
+                                        ->dehydrated(false),
+                                    Forms\Components\Hidden::make('taxonomy_terms')
+                                        ->dehydrateStateUsing(fn (Closure $get) => Arr::flatten($get('taxonomies') ?? [], 1)),
+                                ])
+                                ->when(fn () => !empty($taxonomies->toArray())),
+                        ]),
                     MetaDataForm::make('Meta Data'),
                 ])->columnSpan(1),
             ]);
@@ -254,7 +267,7 @@ class ProductResource extends Resource
                     ->query(function (Builder $query, array $data) {
                         $query->when(filled($data['value']), function (Builder $query) use ($data) {
                             $query->when(filled($data['value']), function (Builder $query) use ($data) {
-                                /** @var Product|Builder $query */
+                                /** @var Prodct|Builder $query */
                                 match ($data['value']) {
                                     '1' => $query->where('status', true),
                                     '0' => $query->where('status', false),
@@ -278,9 +291,7 @@ class ProductResource extends Resource
 
     public static function getRelations(): array
     {
-        return [
-
-        ];
+        return [];
     }
 
     public static function getPages(): array
