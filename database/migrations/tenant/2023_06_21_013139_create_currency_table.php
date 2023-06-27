e7be6e7cd8eccb4da4f795293d98738af2cfd6a3
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-06-27 13:19:11 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- database/migrations/tenant/2023_06_21_013139_create_currency_table.php
+++ database/migrations/tenant/2023_06_21_013139_create_currency_table.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /** Run the migrations. */
     public function up(): void
     {
         Schema::create('currencies', function (Blueprint $table) {
@@ -22,7 +21,6 @@ return new class () extends Migration {
         });
     }
 
-    /** Reverse the migrations. */
     public function down(): void
     {
         Schema::dropIfExists('currencies');
