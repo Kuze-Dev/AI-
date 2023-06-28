3cc1f03e1ca4836358619f26271a2f2efa697cff
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-06-28 14:02:54 +0800

    Cherry-picked
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

---
+++ domain/Customer/Database/Factories/CustomerFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Customer\Database\Factories;
+
+use Domain\Customer\Enums\Status;
+use Domain\Customer\Models\Customer;
+use Domain\Tier\Database\Factories\TierFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Customer\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    protected $model = Customer::class;
+
+    public function definition(): array
+    {
+        return [
+            'tier_id' => TierFactory::new(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => $this->faker->password(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'mobile' => $this->faker->phoneNumber(),
+            'status' => Arr::random(Status::cases()),
+            'birth_date' => now()->subYears($this->faker->randomDigitNotNull()),
+        ];
+    }
+
+    public function deleted(): self
+    {
+        return $this->state(['deleted_at' => now()]);
+    }
+}
---
+++ domain/Tier/Models/Tier.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Tier\Models;
+
+use Domain\Customer\Models\Customer;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+/**
+ * Domain\Customer\Models\Tier
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Activitylog\Models\Activity> $activities
+ * @property-read int|null $activities_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Domain\Customer\Models\Customer> $customers
+ * @property-read int|null $customers_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier withTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tier withoutTrashed()
+ * @mixin \Eloquent
+ */
+class Tier extends Model
+{
+    use SoftDeletes;
+    use LogsActivity;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logFillable()
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs();
+    }
+
+    /** @return \Illuminate\Database\Eloquent\Relations\HasMany<\Domain\Customer\Models\Customer> */
+    public function customers(): HasMany
+    {
+        return $this->hasMany(Customer::class);
+    }
+}
