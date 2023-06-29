e5fe11d419e86cea7db591426b3e8a5c8b504d18
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-06-29 09:06:41 +0800

    Add password reset notification
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- domain/Customer/Actions/CustomerPasswordResetAction.php
+++ domain/Customer/Actions/CustomerPasswordResetAction.php
@@ -6,14 +6,19 @@ namespace Domain\Customer\Actions;
 
 use Domain\Customer\Models\Customer;
 use Domain\Customer\Notifications\PasswordHasBeenReset;
+use Illuminate\Support\Str;
 
 class CustomerPasswordResetAction
 {
     public function execute(Customer $customer, string $password): void
     {
-        $customer->update([
-            'password' => $password,
-        ]);
+        $customer
+            ->fill([
+                'password' => $password,
+            ])
+            ->setRememberToken(Str::random(60));
+
+        $customer->save();
 
         $customer->notify(new PasswordHasBeenReset());
     }
