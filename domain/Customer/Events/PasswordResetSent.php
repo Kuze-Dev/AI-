968b34a22e306d5a2fd6c398edbdc43cb2adfc84
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-06-29 13:33:04 +0800

    Add log
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- domain/Customer/Events/PasswordResetSent.php
+++ domain/Customer/Events/PasswordResetSent.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Domain\Customer\Events;
 
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
 class PasswordResetSent
 {
     public function __construct(
-        public readonly Authenticatable $user
+        public readonly Authenticatable&Model $user
     ) {
     }
 }
