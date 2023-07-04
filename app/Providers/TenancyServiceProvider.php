779ea55a8a6cbb0200e0d09238af9ebc83453458
Julio Motol <jrmotol.halcyondigital@gmail.com>
2023-07-04 09:37:13 +0800

    🐛 Tenant creation
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- app/Providers/TenancyServiceProvider.php
+++ app/Providers/TenancyServiceProvider.php
@@ -35,16 +35,18 @@ class TenancyServiceProvider extends ServiceProvider
                     /** @var Tenant $tenant */
                     $tenant = $event->tenant;
 
-                    Bus::chain(array_merge([
-                        new CreateDatabase($tenant),
-                        new Jobs\MigrateDatabase($tenant),
+                    Bus::chain(array_merge(
+                        [
+                            new CreateDatabase($tenant),
+                            new Jobs\MigrateDatabase($tenant),
+                        ],
                         ! app()->runningUnitTests()
                             ? [
                                 new Jobs\SeedDatabase($tenant),
                                 new CreateS3Bucket($tenant),
                             ]
                             : [],
-                    ]))->dispatch();
+                    ))->dispatch();
                 },
             ],
             Events\SavingTenant::class => [],
