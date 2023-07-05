2bf52d6c4e0474678774c2898c688dff14202d36
Julio Motol <jrmotol.halcyondigital@gmail.com>
2023-07-05 18:40:12 +0800

    🧪 Add tests
    Signed-off-by: Julio Motol <jrmotol.halcyondigital@gmail.com>

--- tests/Feature/FilamentTenant/Resources/TaxZoneResource/Pages/CreateTaxZoneTest.php
+++ tests/Feature/FilamentTenant/Resources/TaxZoneResource/Pages/CreateTaxZoneTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\FilamentTenant\Resources\TaxZoneResource\Pages\CreateTaxZone;
+use Domain\Address\Database\Factories\CountryFactory;
 use Domain\Taxation\Database\Factories\TaxZoneFactory;
 use Domain\Taxation\Enums\PriceDisplay;
 use Domain\Taxation\Enums\TaxZoneType;
@@ -74,6 +75,36 @@ it('can create and override old default', function () {
     expect($initialDefaultTaxZone->refresh())->is_default->toBeFalse();
 });
 
-todo('can create tax zone by country');
+it('can create tax zone by country', function () {
+    $countries = CountryFactory::new()->count(5)->create();
+    $selectedCountries = $countries->random(2);
+
+    $record = livewire(CreateTaxZone::class)
+        ->assertFormFieldIsHidden('countries')
+        ->fillForm([
+            'name' => 'Test',
+            'price_display' => PriceDisplay::EXCLUSIVE,
+            'type' => TaxZoneType::COUNTRY,
+            'percentage' => 12.000,
+        ])
+        ->assertFormFieldIsVisible('countries')
+        ->fillForm(['countries' => $selectedCountries->modelKeys()])
+        ->call('create')
+        ->assertHasNoFormErrors()
+        ->assertOk()
+        ->instance()
+        ->record;
+
+    assertDatabaseHas(TaxZone::class, [
+        'name' => 'Test',
+        'is_default' => true,
+    ]);
+    foreach ($selectedCountries as $selectedCountry) {
+        assertDatabaseHas('tax_zone_country', [
+            'tax_zone_id' => $record->getKey(),
+            'country_id' => $selectedCountry->getKey(),
+        ]);
+    }
+});
 
 todo('can create tax zone by state');
