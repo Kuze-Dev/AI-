5844ffd7f29cf6028af4aafce1c66f3556f2dc6a
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-07-06 13:26:27 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

---
+++ domain/Address/Models/Address.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Address\Models;
+
+use Domain\Customer\Models\Customer;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Arr;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+/**
+ * Domain\Customer\Models\Tier
+ *
+ * @property int $id
+ * @property int $customer_id
+ * @property int $state_id
+ * @property string $address_line_1
+ * @property string $zip_code
+ * @property string $city
+ * @property bool $is_default_shipping
+ * @property bool $is_default_billing
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Activitylog\Models\Activity> $activities
+ * @property-read int|null $activities_count
+ * @property-read Customer|null $customer
+ * @property-read string $full_detail
+ * @property-read \Domain\Address\Models\State|null $state
+ * @method static \Illuminate\Database\Eloquent\Builder|Address newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Address newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Address query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereAddressLine1($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereCustomerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereIsDefaultBilling($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereIsDefaultShipping($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereStateId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereZipCode($value)
+ * @mixin \Eloquent
+ */
+class Address extends Model
+{
+    use LogsActivity;
+
+    protected $fillable = [
+        'customer_id',
+        'state_id',
+        'address_line_1',
+        'zip_code',
+        'city',
+        'is_default_shipping',
+        'is_default_billing',
+    ];
+
+    protected $casts = [
+        'is_default_billing' => 'bool',
+        'is_default_shipping' => 'bool',
+    ];
+
+    /** @return Attribute<string, never> */
+    protected function fullDetail(): Attribute
+    {
+        return Attribute::get(
+            fn ($value): string => Arr::join(
+                array_filter([
+                    $this->address_line_1,
+                    $this->state->country->name,
+                    $this->state->name,
+                    $this->zip_code,
+                    $this->city,
+                ]),
+                ', '
+            )
+        );
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logFillable()
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs();
+    }
+
+    /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\Domain\Customer\Models\Customer, \Domain\Address\Models\Address> */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\Domain\Address\Models\State, \Domain\Address\Models\Address> */
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+}
---
+++ domain/Customer/Models/Customer.php
@@ -0,0 +1,150 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Customer\Models;
+
+use Domain\Address\Models\Address;
+use Domain\Customer\Notifications\VerifyEmail;
+use Domain\Customer\Enums\Status;
+use Domain\Tier\Models\Tier;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+/**
+ * Domain\Customer\Models\Customer
+ *
+ * @property int $id
+ * @property int $tier_id
+ * @property string $cuid customer unique ID
+ * @property string $email
+ * @property mixed|null $password
+ * @property-read  string $full_name
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $mobile
+ * @property Status $status
+ * @property \Illuminate\Support\Carbon $birth_date
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Activitylog\Models\Activity> $activities
+ * @property-read int|null $activities_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, Address> $addresses
+ * @property-read int|null $addresses_count
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property-read int|null $media_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read Tier $tier
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereBirthDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereCuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereMobile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereTierId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer withTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer withoutTrashed()
+ * @mixin \Eloquent
+ */
+class Customer extends Authenticatable implements HasMedia, MustVerifyEmail
+{
+    use SoftDeletes;
+    use LogsActivity;
+    use InteractsWithMedia;
+    use Notifiable;
+    use HasApiTokens;
+
+    protected $fillable = [
+        'tier_id',
+        'cuid',
+        'email',
+        'password',
+        'first_name',
+        'last_name',
+        'mobile',
+        'status',
+        'birth_date',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+        'birth_date' => 'date',
+        'status' => Status::class,
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function getRouteKeyName(): string
+    {
+        return 'cuid';
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logFillable()
+            ->logOnlyDirty()
+            ->logExcept(['password'])
+            ->dontSubmitEmptyLogs();
+    }
+
+    /** @return Attribute<string, never> */
+    protected function fullName(): Attribute
+    {
+        return Attribute::get(
+            fn ($value): string => "{$this->first_name} {$this->last_name}"
+        );
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('image')
+            ->singleFile();
+    }
+
+    /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\Domain\Tier\Models\Tier, \Domain\Customer\Models\Customer> */
+    public function tier(): BelongsTo
+    {
+        return $this->belongsTo(Tier::class);
+    }
+
+    /** @return \Illuminate\Database\Eloquent\Relations\HasMany<\Domain\Address\Models\Address> */
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(Address::class);
+    }
+
+    public function sendEmailVerificationNotification(): void
+    {
+        $this->notify(new VerifyEmail());
+    }
+}
