190bcbf5edc83d34e9efa5303e3a0f00132dd17e
benedict <benedict.halcyondigital@gmail.com>
2023-07-11 16:13:52 +0800

    remove cart seeder

--- database/seeders/Tenant/DatabaseSeeder.php
+++ database/seeders/Tenant/DatabaseSeeder.php
@@ -24,7 +24,6 @@ class DatabaseSeeder extends Seeder
             Address\CountrySeeder::class,
             Currency\CurrencySeeder::class,
             Tier\TierSeeder::class,
-            Cart\CartSeeder::class,
         ]);
     }
 }
