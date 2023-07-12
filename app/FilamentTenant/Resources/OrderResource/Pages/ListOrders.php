1c49f35df2c182b4f4daadad15df7f8b2fb61663
benedict <benedict.halcyondigital@gmail.com>
2023-07-12 11:27:24 +0800

    table with tab headers

--- app/FilamentTenant/Resources/OrderResource/Pages/ListOrders.php
+++ app/FilamentTenant/Resources/OrderResource/Pages/ListOrders.php
@@ -5,17 +5,34 @@ declare(strict_types=1);
 namespace App\FilamentTenant\Resources\OrderResource\Pages;
 
 use App\FilamentTenant\Resources\OrderResource;
-use Filament\Pages\Actions;
+use App\FilamentTenant\Support\Concerns\HasTabHeader;
+use App\FilamentTenant\Support\Contracts\HasTabHeader as ContractsHasTabHeader;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
-class ListOrders extends ListRecords
+class ListOrders extends ListRecords implements ContractsHasTabHeader
 {
+    use HasTabHeader;
+
     protected static string $resource = OrderResource::class;
 
-    protected function getActions(): array
+    public function getTabOptions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            "All", "Pending", "Packed", "Shipped", "Fulfilled", "For Cancellation", "Cancelled", "Refunded"
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        $query = parent::getTableQuery();
+
+        $option = $this->activeOption;
+
+        if ($option != "All") {
+            $query->where('status', $option);
+        }
+
+        return $query;
     }
 }
