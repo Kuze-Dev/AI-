740bade2e87a2d09ece11f6d14b6f1ebb287bd4d
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-07-12 08:04:57 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -102,7 +102,7 @@ class AuthServiceProvider extends ServiceProvider
                 $baseUrl = app(ECommerceSettings::class)->domainWithScheme()
                     ?? app(SiteSettings::class)->domainWithScheme();
 
-                return $baseUrl.'/password-reset'.'?'.http_build_query([
+                return $baseUrl.'/password/reset'.'?'.http_build_query([
                     'token' => $token,
                     'email' => $notifiable->getEmailForPasswordReset(),
                 ]);
