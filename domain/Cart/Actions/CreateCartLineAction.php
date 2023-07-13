4d3c9f22f00a54f97cb08b029008b94ead58823d
benedict <benedict.halcyondigital@gmail.com>
2023-07-14 06:53:42 +0800

    fix allowing remarks

--- domain/Cart/Actions/CreateCartLineAction.php
+++ domain/Cart/Actions/CreateCartLineAction.php
@@ -49,9 +49,8 @@ class CreateCartLineAction
                 ]
             );
 
-            $cartLine->clearMediaCollection('cart_line_notes');
-
             if ($cartLineData->medias !== null) {
+                $cartLine->clearMediaCollection('cart_line_notes');
                 foreach ($cartLineData->medias as $imageUrl) {
                     try {
                         $cartLine->addMediaFromUrl($imageUrl)
