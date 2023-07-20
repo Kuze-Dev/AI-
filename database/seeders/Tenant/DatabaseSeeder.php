10dc84e92a89f21188654fc08f846b7bd0f77c08
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-07-21 07:48:14 +0800

    Add default value on site settings
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- database/seeders/Tenant/DatabaseSeeder.php
+++ database/seeders/Tenant/DatabaseSeeder.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Database\Seeders\Tenant;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Console\OptimizeClearCommand;
 use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Artisan::call('permission:cache-reset');
+        Artisan::call(OptimizeClearCommand::class);
 
         $this->call([
             Auth\PermissionSeeder::class,
