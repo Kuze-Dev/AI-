511403aa414c8d9a517dd3f5a40ceb2766cdd916
Jerome Hipolito <jeromehipolito.halcyondigital@gmail.com>
2023-07-20 09:47:38 +0800

    product cms review

--- app/FilamentTenant/Resources/ReviewResource/RelationManagers/ReviewRelationManager.php
+++ app/FilamentTenant/Resources/ReviewResource/RelationManagers/ReviewRelationManager.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\FilamentTenant\Resources\ReviewResource\RelationManagers;
 
-use App\FilamentTenant\Resources\ReviewResource;
 use Filament\Resources\Form;
+use Filament\Forms;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
 use Filament\Tables;
@@ -20,22 +20,34 @@ class ReviewRelationManager extends RelationManager
 
     public static function form(Form $form): Form
     {
-        return ReviewResource::form($form);
+        return $form
+            ->schema([
+                Forms\Components\Fieldset::make('customer')->relationship('customer')->schema([
+                    Forms\Components\TextInput::make('first_name')->default('Anonymous'),
+                    Forms\Components\TextInput::make('email')->default('Anonymous'),
+                ]),
+                Forms\Components\TextInput::make('comment'),
+                Forms\Components\TextInput::make('rating'),
+            ])->columns(1);
     }
-
     public static function table(Table $table): Table
     {
         return $table
         ->columns([
+            Tables\Columns\TextColumn::make('Customer.first_name')->default('Anonymous')
+            ->label('Name')
+            ->sortable(),
             Tables\Columns\TextColumn::make('rating')
             ->label('Rating')
             ->sortable(),
             Tables\Columns\TextColumn::make('comment')
-                ->label('Comment')
-                ->sortable(),
+            ->label('Comment') 
+            ->sortable(),
+     
         ])
         ->actions([
-            Tables\Actions\ViewAction::make(),
+           Tables\Actions\ViewAction::make(),   
+           Tables\Actions\DeleteAction::make()
         ])
         ->bulkActions([])
         ->defaultSort('id', 'asc');
