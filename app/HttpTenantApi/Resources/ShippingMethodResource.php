17fed28533cba7f0974e76d65d4f2194a07e351c
Roy Biscocho <rbiscocho.halcyondigital@gmail.com>
2023-07-21 13:18:32 +0800

    add shipping method api list
    Signed-off-by: Roy Biscocho <rbiscocho.halcyondigital@gmail.com>

---
+++ app/HttpTenantApi/Resources/ShippingMethodResource.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\HttpTenantApi\Resources;
+
+use Illuminate\Http\Request;
+use TiMacDonald\JsonApi\JsonApiResource;
+
+/**
+ * @mixin \Domain\ShippingMethod\Models\ShippingMethod
+ */
+class ShippingMethodResource extends JsonApiResource
+{
+    public function toAttributes(Request $request): array
+    {
+        return  [
+            'name' => $this->title,
+            'slug' => $this->slug,
+            'subtitle' => $this->subtitle,
+            'description' => $this->description,
+            'driver' => $this->driver,
+            'ship_from_address' => $this->ship_from_address,
+            'status' => $this->status,
+
+        ];
+    }
+
+    public function toRelationships(Request $request): array
+    {
+        return [
+
+        ];
+    }
+}
