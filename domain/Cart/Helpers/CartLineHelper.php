7969724a786d31dbc890114387f09c570b364b42
benedict-wfh <benedict.halcyondigital@gmail.com>
2023-07-24 08:02:17 +0800

    composer format

--- domain/Cart/Helpers/CartLineHelper.php
+++ domain/Cart/Helpers/CartLineHelper.php
@@ -115,7 +115,7 @@ class CartLineHelper
         $taxPercentage = (float) $taxZone->percentage;
         $taxDisplay = $taxZone->price_display;
 
-        if (!$taxZone instanceof TaxZone) {
+        if ( ! $taxZone instanceof TaxZone) {
             throw new BadRequestHttpException('No tax zone found');
         }
 
@@ -130,7 +130,7 @@ class CartLineHelper
     {
         $discountTotal = 0;
 
-        if (!is_null($discount)) {
+        if ( ! is_null($discount)) {
             $discountTotal = (new DiscountHelperFunctions())->deductOrderSubtotal($discount, $subTotal);
         }
 
