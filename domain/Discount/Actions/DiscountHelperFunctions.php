1b35a9c6a2310027fb5d540225e2f889743b62d8
Darwin Cabarrubias <darwin.halcyondigital@gmail.com>
2023-07-24 10:36:56 +0800

    wip

--- domain/Discount/Actions/DiscountHelperFunctions.php
+++ domain/Discount/Actions/DiscountHelperFunctions.php
@@ -7,24 +7,31 @@ namespace Domain\Discount\Actions;
 use Domain\Discount\Enums\DiscountAmountType;
 use Domain\Discount\Enums\DiscountConditionType;
 use Domain\Discount\Models\Discount;
+use Exception;
 
 final class DiscountHelperFunctions
 {
     public function deductOrderSubtotal(Discount $discount, float $subTotal): ?float
     {
 
-        if (
-            $discount->discountCondition->discount_type === DiscountConditionType::ORDER_SUB_TOTAL
-            && $subTotal >= $discount->discountRequirement?->minimum_amount
-        ) {
-            if ($discount->discountCondition->amount_type === DiscountAmountType::FIXED_VALUE) {
-                return $discount->discountCondition->amount;
-            } elseif ($discount->discountCondition->amount_type === DiscountAmountType::PERCENTAGE) {
-                $deductable = $subTotal * ($discount->discountCondition->amount / 100);
+        try {
+            if (
+                $discount->discountCondition->discount_type === DiscountConditionType::ORDER_SUB_TOTAL
+                && $subTotal >= $discount->discountRequirement?->minimum_amount
+            ) {
+                if ($discount->discountCondition->amount_type === DiscountAmountType::FIXED_VALUE) {
+                    return $discount->discountCondition->amount;
+                } elseif ($discount->discountCondition->amount_type === DiscountAmountType::PERCENTAGE) {
+                    $deductable = $subTotal * ($discount->discountCondition->amount / 100);
 
-                return $deductable;
+                    return $deductable;
+                }
+            } else {
+                throw new Exception('Minimum amount requirement not met for the discount.');
             }
-        } else {
+        } catch (Exception $e) {
+            // Handle the exception here or log it for further investigation.
+            // You can return null or any appropriate value as needed.
             return 0;
         }
     }
