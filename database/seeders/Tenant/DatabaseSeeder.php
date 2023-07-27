dcc7ce61d520fc4aef8e111772d44ddba95d8044
Roy Biscocho <rbiscocho.halcyondigital@gmail.com>
2023-07-27 14:43:08 +0800

    register shippingboxSeeder
    Signed-off-by: Roy Biscocho <rbiscocho.halcyondigital@gmail.com>

--- database/seeders/Tenant/DatabaseSeeder.php
+++ database/seeders/Tenant/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             Address\CountrySeeder::class,
             Tier\TierSeeder::class,
             ShippingMethod\ShippingMethodSeeder::class,
+            ShippingMethod\ShippingBoxSeeder::class,
         ]);
     }
 }
