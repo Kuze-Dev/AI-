bcbdba89f923ddc9ccbf9f3e94373c643a088393
Marlon Nino <mvnino.halcyondigital@gmail.com>
2023-08-01 11:02:54 +0800

    feat: update product auth controller

--- app/HttpTenantApi/Controllers/Product/ProductAuthController.php
+++ app/HttpTenantApi/Controllers/Product/ProductAuthController.php
@@ -4,15 +4,8 @@ declare(strict_types=1);
 
 namespace App\HttpTenantApi\Controllers\Product;
 
-use App\HttpTenantApi\Resources\ProductResource;
-use Domain\Product\Models\Builders\ProductBuilder;
-use Domain\Product\Models\Product;
-use Illuminate\Support\Arr;
-use Spatie\QueryBuilder\AllowedFilter;
-use Spatie\QueryBuilder\QueryBuilder;
 use Spatie\RouteAttributes\Attributes\ApiResource;
 use Spatie\RouteAttributes\Attributes\Middleware;
-use TiMacDonald\JsonApi\JsonApiResourceCollection;
 
 #[
     ApiResource('auth/products', only: ['index', 'show']),
