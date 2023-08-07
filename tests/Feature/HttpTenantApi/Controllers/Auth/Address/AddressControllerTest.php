b98f9867114316f72e16b4bbaebe703796e903a4
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-08-07 13:39:33 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

---
+++ tests/Feature/HttpTenantApi/Controllers/Auth/Address/AddressControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+uses()->group('customer');
+
+beforeEach(function () {
+    testInTenantContext();
+});
+
+todo('can list');
+todo('can create');
+
+it('can create then replace new default', function () {
+
+})
+    ->with(['shipping', 'billing']);
+
+todo('can edit');
+todo('can delete');
