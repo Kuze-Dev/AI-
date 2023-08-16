6ca777bd2dfce47b6cb0e5c4bdea69649ff05c32
Darwin Cabarrubias <darwin.halcyondigital@gmail.com>
2023-08-16 08:33:03 +0800

    used config instead of tenancy

--- domain/Admin/Notifications/VerifyEmail.php
+++ domain/Admin/Notifications/VerifyEmail.php
@@ -15,7 +15,7 @@ class VerifyEmail extends \Illuminate\Auth\Notifications\VerifyEmail implements 
     protected function buildMailMessage($url)
     {
         return (new MailMessage())
-            ->subject(trans('Please Verify your email address for your '.tenancy()->tenant?->name.' website login'))
+            ->subject(trans('Please Verify your email address for your '.config('app.name').' website login'))
             ->line(trans('Please click the button below to verify your email address.'))
             ->action(trans('Verify Email Address'), $url)
             ->line(trans('If you did not create an account, no further action is required.'));
