b723b649541753cf7d7a5bafa5bfd15da982e651
Roy Biscocho <rbiscocho.halcyondigital@gmail.com>
2023-09-05 11:00:56 +0800

    wip
    Signed-off-by: Roy Biscocho <rbiscocho.halcyondigital@gmail.com>

--- database/settings/2022_10_18_071347_create_general_settings.php
+++ database/settings/2022_10_18_071347_create_general_settings.php
@@ -10,7 +10,7 @@ return new class () extends SettingsMigration {
         $this->migrator->add('site.name', config('app.name'));
         $this->migrator->add('site.description', 'We will be using TALL (Tailwind, Alpine.JS, Livewire, and Laravel) stack to this new boilerplate.');
         $this->migrator->add('site.author', 'Halcyon AGILE');
-        $this->migrator->add('site.logo', 'logo.png');
-        $this->migrator->add('site.favicon', 'favicon.png');
+        $this->migrator->add('site.logo', '');
+        $this->migrator->add('site.favicon', '');
     }
 };
