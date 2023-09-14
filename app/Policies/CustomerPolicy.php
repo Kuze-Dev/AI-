00b8025f114e89b61761fd6eddbaea0c422b1c90
Darwin G Cabarrubias <darwin.halcyondigital@gmail.com>
2023-09-14 21:47:41 +0800

    customer moved in feature selector

--- app/Policies/CustomerPolicy.php
+++ app/Policies/CustomerPolicy.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Policies;
 
-use App\Features\ECommerce\ECommerceBase;
+use App\Features\Customer\CustomerBase;
 use App\Policies\Concerns\ChecksWildcardPermissions;
 use Illuminate\Foundation\Auth\User;
 use Domain\Customer\Models\Customer;
@@ -16,7 +16,7 @@ class CustomerPolicy
 
     public function before(): ?Response
     {
-        if ( ! tenancy()->tenant?->features()->active(ECommerceBase::class)) {
+        if ( ! tenancy()->tenant?->features()->active(CustomerBase::class)) {
             return Response::denyAsNotFound();
         }
 
