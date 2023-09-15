c0ddb6fa943ab011ea32ab22efc8175c5de592b2
Darwin Cabarrubias <darwin.halcyondigital@gmail.com>
2023-09-15 15:46:09 +0800

    tier moved to request form

--- app/HttpTenantApi/Controllers/Auth/Customer/RegisterController.php
+++ app/HttpTenantApi/Controllers/Auth/Customer/RegisterController.php
@@ -10,7 +10,6 @@ use App\HttpTenantApi\Resources\CustomerResource;
 use App\Notifications\Customer\NewRegisterNotification;
 use Domain\Customer\Actions\CreateCustomerAction;
 use Domain\Customer\DataTransferObjects\CustomerData;
-use Domain\Tier\Models\Tier;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Notification;
 use Spatie\RouteAttributes\Attributes\Middleware;
@@ -24,12 +23,10 @@ class RegisterController
     #[Post('register', name: 'customer.register')]
     public function __invoke(CustomerRegisterRequest $request): CustomerResource
     {
-        /** @var \Domain\Tier\Models\Tier $tier */
-        $tier = Tier::whereName(config('domain.tier.default'))->first();
 
         $customer = DB::transaction(
             fn () => app(CreateCustomerAction::class)
-                ->execute(CustomerData::fromRegistrationRequest($tier, $request))
+                ->execute(CustomerData::fromRegistrationRequest($request))
         );
 
         Notification::send($customer, new NewRegisterNotification($customer));
