24345e1cf43a30f7a724bf1029b26358eabaa292
Roy Biscocho <rbiscocho.halcyondigital@gmail.com>
2023-10-02 09:27:12 +0800

    wip
    Signed-off-by: Roy Biscocho <rbiscocho.halcyondigital@gmail.com>

--- support/ApiCall/Actions/CreateApiCallLogAction.php
+++ support/ApiCall/Actions/CreateApiCallLogAction.php
@@ -11,10 +11,37 @@ class CreateApiCallLogAction
 {
     public function execute(ApiCallData $data): void
     {
+
         ApiCall::create([
             'request_url' => $data->requestUrl,
             'request_type' => $data->requestType,
             'request_response' => $data->requestResponse,
         ]);
+
+        $tenant = tenancy()->tenant;
+
+        if ($tenant) {
+           
+            $today = now()->format('Y-m-d');
+
+            /** @var \Domain\Tenant\Models\TenantApiCall|null */
+            $tenantLog = $tenant->apiCalls()->where(
+              'date', $today
+            )->first();
+
+            if ($tenantLog) {
+
+                $tenantLog->update([
+                    'count' => $tenantLog->count += 1
+                ]);
+
+            } else {
+                $tenant->apiCalls()->create([
+                    'date' => $today,
+                    'count' => 1,
+                ]);
+            }
+        }
+        
     }
 }
