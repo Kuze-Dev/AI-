1ba5dc092e2b8012a2784903e175aef04faf2a56
Jade Soriano <jade.halcyondigital@gmail.com>
2023-10-04 13:57:21 +0800

    wip: get billing and due date
    Signed-off-by: Jade Soriano <jade.halcyondigital@gmail.com>

---
+++ domain/ServiceOrder/Actions/GetServiceOrderBillingAndDueDateAction.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\ServiceOrder\Actions;
+
+use Carbon\Carbon;
+use Domain\Service\Enums\BillingCycle;
+use Domain\ServiceOrder\DataTransferObjects\ServiceOrderBillingAndDueDateData;
+use Domain\ServiceOrder\Exceptions\InvalidServiceBillingCycleException;
+use Domain\ServiceOrder\Models\ServiceOrder;
+use Throwable;
+
+class GetServiceOrderBillingAndDueDateAction
+{
+    /** @throws Throwable */
+    public function execute(
+        ServiceOrder $serviceOrder,
+        Carbon $date
+    ): mixed {
+        $referenceDate = now()->parse($date);
+
+        $billDate = match ($serviceOrder->billing_cycle) {
+            BillingCycle::DAILY => $referenceDate->addDay(),
+            BillingCycle::MONTHLY => $referenceDate->addMonth(),
+            BillingCycle::YEARLY => $referenceDate->addYear(),
+            /** @phpstan-ignore-next-line  */
+            default => throw new InvalidServiceBillingCycleException()
+        };
+
+        return new ServiceOrderBillingAndDueDateData(
+            bill_date: $billDate,
+            due_date: $billDate->addDays($serviceOrder->due_date_every)
+        );
+    }
+}
