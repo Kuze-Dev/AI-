5e0aaaceab860e81c4389dea788d9f9ab1e4c1a2
Roy Biscocho <rbiscocho.halcyondigital@gmail.com>
2023-10-04 08:27:47 +0800

    trial only
    Signed-off-by: Roy Biscocho <rbiscocho.halcyondigital@gmail.com>

--- app/Tenancy/Bootstrappers/BucketBootstrapper.php
+++ app/Tenancy/Bootstrappers/BucketBootstrapper.php
@@ -11,19 +11,34 @@ use Stancl\Tenancy\Contracts\Tenant;
 class BucketBootstrapper implements TenancyBootstrapper
 {
     protected ?string $orignalBucket;
+    protected ?string $originalAwsEndpoint;
+    protected ?bool $originalUsePathStyleEndpoint;
 
     public function __construct(protected Application $app)
     {
         $this->orignalBucket = $this->app->make('config')['filesystems.disks.s3.bucket'];
+
+        $this->originalAwsEndpoint = $this->app->make('config')['filesystems.disks.s3.endpoint'];
+
+        $this->originalUsePathStyleEndpoint = $this->app->make('config')['filesystems.disks.s3.use_path_style_endpoint'];
     }
 
     public function bootstrap(Tenant $tenant): void
     {
         $this->app->make('config')->set('filesystems.disks.s3.bucket', $tenant->getInternal('bucket'));
+
+        if ($tenant->getInternal('aws_endpoint')) {
+            $this->app->make('config')->set('filesystems.disks.s3.endpoint', $tenant->getInternal('aws_endpoint'));
+            $this->app->make('config')->set('filesystems.disks.s3.use_path_style_endpoint', true);
+        }
     }
 
     public function revert(): void
     {
         $this->app->make('config')->set('filesystems.disks.s3.bucket', $this->orignalBucket);
+
+        $this->app->make('config')->set('filesystems.disks.s3.endpoint', $this->originalAwsEndpoint);
+
+        $this->app->make('config')->set('filesystems.disks.s3.use_path_style_endpoint', $this->originalUsePathStyleEndpoint);
     }
 }
