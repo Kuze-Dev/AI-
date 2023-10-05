9f7e3ce41aee8c43d82b015f49dba902c853f0d0
Darwin Cabarrubias <darwin.halcyondigital@gmail.com>
2023-10-05 10:28:18 +0800

    refactor error message on invalid user

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -19,6 +19,6 @@ return [
     'sent' => 'We have emailed your password reset link!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'user' => "Password reset link has been sent your email!",
 
 ];
