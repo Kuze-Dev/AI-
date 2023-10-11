296e5abfbbed2250be8d0551c4dc274599db036b
Darwin Cabarrubias <darwin.halcyondigital@gmail.com>
2023-10-11 19:45:45 +0800

    refactor

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -171,17 +171,5 @@ class AuthServiceProvider extends ServiceProvider
                 );
             }
         });
-        // ResetPasswordNotification::toMailUsing(function (mixed $notifiable, $url): MailMessage {
-
-        //     return (new MailMessage)
-        //     ->from(app(FormSettings::class)->sender_email ?? config('mail.from.address'))
-        //     ->subject(trans('Reset Password Notification'))
-        //     ->line(trans('You are receiving this email because we received a password reset request for your account.'))
-        //     ->action(trans('Reset Password'), $url)
-        //     ->line(trans('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-        //     ->line(trans('If you did not request a password reset, no further action is required.'));
-
-        // });
-
     }
 }
