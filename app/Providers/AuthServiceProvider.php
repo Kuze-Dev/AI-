abb3a694790756a82a1ed6f43377580ecc991108
Darwin Cabarrubias <darwin.halcyondigital@gmail.com>
2023-10-11 18:20:59 +0800

    wip

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Settings\ECommerceSettings;
+use App\Settings\FormSettings;
 use App\Settings\SiteSettings;
 use Domain\Admin\Models\Admin;
 use Domain\Auth\Contracts\HasEmailVerificationOTP;
@@ -72,6 +73,7 @@ class AuthServiceProvider extends ServiceProvider
 
             if ($notifiable instanceof HasEmailVerificationOTP && $notifiable->isEmailVerificationUseOTP()) {
                 return (new MailMessage())
+                    ->from(app(FormSettings::class)->sender_email ?? config('mail.from.address'))
                     ->subject(trans('Verify Email Address'))
                     ->line(trans('Please copy OTP below to verify your email address.'))
                     ->line('OTP: ' . $notifiable->generateEmailVerificationOTP())
@@ -81,6 +83,7 @@ class AuthServiceProvider extends ServiceProvider
             // copied from \Illuminate\Auth\Notifications\VerifyEmail::buildMailMessage($url)
             // https://github.com/laravel/framework/blob/v10.16.1/src/Illuminate/Auth/Notifications/VerifyEmail.php#L62
             return (new MailMessage())
+                ->from(app(FormSettings::class)->sender_email ?? config('mail.from.address'))
                 ->subject(trans('Verify Email Address'))
                 ->line(trans('Please click the button below to verify your email address.'))
                 ->action(trans('Verify Email Address'), $url)
@@ -133,7 +136,6 @@ class AuthServiceProvider extends ServiceProvider
                 );
             }
         });
-
         ResetPasswordNotification::createUrlUsing(function (mixed $notifiable, string $token) {
 
             if ($notifiable instanceof Customer) {
@@ -169,5 +171,17 @@ class AuthServiceProvider extends ServiceProvider
                 );
             }
         });
+        // ResetPasswordNotification::toMailUsing(function (mixed $notifiable, $url): MailMessage {
+
+        //     return (new MailMessage)
+        //     ->from(app(FormSettings::class)->sender_email ?? config('mail.from.address'))
+        //     ->subject(trans('Reset Password Notification'))
+        //     ->line(trans('You are receiving this email because we received a password reset request for your account.'))
+        //     ->action(trans('Reset Password'), $url)
+        //     ->line(trans('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+        //     ->line(trans('If you did not request a password reset, no further action is required.'));
+
+        // });
+
     }
 }
