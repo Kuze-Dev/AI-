ea6d5c889dd957b24948fb5d8ad000ca7d405070
benedict <benedict.halcyondigital@gmail.com>
2023-10-12 09:55:23 +0800

    fix when tax is inclusive

--- app/HttpTenantApi/Controllers/Cart/PublicCart/GuestCartSummaryController.php
+++ app/HttpTenantApi/Controllers/Cart/PublicCart/GuestCartSummaryController.php
@@ -82,8 +82,7 @@ class GuestCartSummaryController extends Controller
 
             $responseArray = [
                 'tax' => [
-                    'inclusive_sub_total' => $summary->taxTotal ? number_format((float) ($summary->subTotal + $summary->taxTotal), 2, '.', ',') : null,
-                    'display' => $summary->taxTotal ? $summary->taxDisplay : null,
+                    'display' => $summary->taxDisplay,
                     'percentage' => $summary->taxPercentage ? round($summary->taxPercentage, 2) : 0,
                     'amount' => $summary->taxTotal ? number_format((float) $summary->taxTotal, 2, '.', ',') : 0,
                 ],
