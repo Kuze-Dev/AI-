b5fe1d1db0a30d8a955883f1e8534ff31e38ba53
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-10-19 11:14:49 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- app/FilamentTenant/Resources/ServiceOrderResource.php
+++ app/FilamentTenant/Resources/ServiceOrderResource.php
@@ -604,18 +604,6 @@ class ServiceOrderResource extends Resource
 
                             try {
                                 /**
-                                 * Requirements:
-                                 *
-                                 * run: npm install puppeteer
-                                 * run: sudo apt-get install -y nodejs gconf-service libasound2 libatk1.0-0 libc6
-                                 * libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libgconf-2-4
-                                 * libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0
-                                 * libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6
-                                 * libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation
-                                 * libappindicator1 libnss3 lsb-release xdg-utils wget libgbm-dev libxshmfence-dev
-                                 * run: sudo npm install --location=global --unsafe-perm puppeteer
-                                 * run: sudo chmod -R o+rx /usr/lib/node_modules/puppeteer/.local-chromium
-                                 *
                                  * Docs: https://spatie.be/docs/browsershot/v2/requirements
                                  */
                                 Browsershot::html(
