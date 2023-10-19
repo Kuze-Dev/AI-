9a429d1b7542cbc6e55b99aa6d7a9a14491038ec
Jade Soriano <jade.halcyondigital@gmail.com>
2023-10-19 10:12:48 +0800

    wip: custom media path generator
    Signed-off-by: Jade Soriano <jade.halcyondigital@gmail.com>

---
+++ app/MediaPathGenerator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Domain\Customer\Models\Customer;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\DefaultPathGenerator;
+
+class MediaPathGenerator extends DefaultPathGenerator
+{
+    protected function getBasePath(Media $media): string
+    {
+        if ($media->model instanceof Customer) {
+            return self::forCustomerReceipt($media);
+        }
+
+        return parent::getBasePath($media);
+    }
+
+    public static  function forCustomerReceipt(Media $media): string
+    {
+        if ($media->collection_name !== 'receipts') {
+            return parent::getBasePath($media);
+        }
+
+        $prefix = config('media-library.prefix', '');
+
+        /** @var \Illuminate\Support\Carbon $createdAt */
+        $createdAt = $media->created_at;
+
+        if ($prefix !== '') {
+            return $prefix . '/' . md5(
+                    $createdAt->toString(),
+                    $media->getKey().
+                    $media->model->getKey()
+                );
+        }
+
+        return md5($media->getKey());
+    }
+}
