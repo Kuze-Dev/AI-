16d933626ead5005f66a9c458c61edc1c93e34f2
Marlon Nino <mvnino.halcyondigital@gmail.com>
2023-10-23 17:20:27 +0800

    feat: wip on batch update

--- app/FilamentTenant/Resources/ProductResource/Pages/ListProducts.php
+++ app/FilamentTenant/Resources/ProductResource/Pages/ListProducts.php
@@ -13,6 +13,7 @@ use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Support\Excel\Actions\ExportAction;
 use Illuminate\Database\Eloquent\Builder;
+use Support\Excel\Actions\ImportAction;
 
 class ListProducts extends ListRecords
 {
@@ -22,36 +23,61 @@ class ListProducts extends ListRecords
     {
         return [
             ImportProductAction::proceed(),
+            ImportAction::make('Upload for Batch Update'),
             ExportAction::make()
                 ->model(Product::class)
                 ->queue()
-                ->query(fn (Builder $query) => $query->latest())
+                ->query(fn (Builder $query) => $query->with('productVariants')->latest())
                 ->mapUsing(
                     [
-                        'Name', 'SKU', 'Description', 'Retail Price', 'Selling Price', 'Stock',
-                        'Status', 'Is Digital Product', 'Is Featured', 'Is Special Offer',
-                        'Allow Customer Remarks', 'Weight', 'Dimension', 'Minimum Order Quantity',
-                        'Created At',
+                        'Product ID', 'Is Variant', 'Variant ID', 'Name', 'Variant Combination', 'SKU',
+                        'Retail Price', 'Selling Price', 'Stock', 'Status', 'Is Digital Product',
+                        'Is Featured', 'Is Special Offer', 'Allow Customer Remarks', 'Allow Stocks',
+                        'Allow Guest Purchase', 'Weight', 'Dimension', 'Minimum Order Quantity',
                     ],
-                    fn (Product $product): array => [
-                        $product->name,
-                        $product->sku,
-                        $product->description,
-                        $product->retail_price,
-                        $product->selling_price,
-                        $product->stock,
-                        $product->status ? Status::ACTIVE->value : STATUS::INACTIVE->value,
-                        $product->is_digital_product ? Decision::YES->value : Decision::NO->value,
-                        $product->is_featured ? Decision::YES->value : Decision::NO->value,
-                        $product->is_special_offer ? Decision::YES->value : Decision::NO->value,
-                        $product->allow_customer_remarks ? Decision::YES->value : Decision::NO->value,
-                        $product->allow_stocks ? Decision::YES->value : Decision::NO->value,
-                        $product->allow_guest_purchase ? Decision::YES->value : Decision::NO->value,
-                        $product->weight,
-                        $product->dimension,
-                        $product->minimum_order_quantity,
-                        $product->created_at?->format(config('tables.date_time_format')),
-                    ]
+                    function (Product $product) {
+                        $a = [
+                            [
+                                $product->id,
+                                'no',
+                                '',
+                                $product->name,
+                                '',
+                                $product->sku,
+                                $product->retail_price,
+                                $product->selling_price,
+                                $product->stock,
+                                $product->status ? Status::ACTIVE->value : STATUS::INACTIVE->value,
+                                $product->is_digital_product ? Decision::YES->value : Decision::NO->value,
+                                $product->is_featured ? Decision::YES->value : Decision::NO->value,
+                                $product->is_special_offer ? Decision::YES->value : Decision::NO->value,
+                                $product->allow_customer_remarks ? Decision::YES->value : Decision::NO->value,
+                                $product->allow_stocks ? Decision::YES->value : Decision::NO->value,
+                                $product->allow_guest_purchase ? Decision::YES->value : Decision::NO->value,
+                                $product->weight,
+                                $product->dimension,
+                                $product->minimum_order_quantity,
+                            ],
+                        ];
+                        foreach ($product->productVariants as $variant) {
+                            $a[] =
+                                [
+                                    $variant->product_id,
+                                    'yes',
+                                    $variant->id,
+                                    '',
+                                    $variant->combination,
+                                    $variant->sku,
+                                    $variant->retail_price,
+                                    $variant->selling_price,
+                                    $variant->stock,
+                                    $variant->status ? Status::ACTIVE->value : STATUS::INACTIVE->value,
+
+                                ];
+                        }
+
+                        return $a;
+                    }
                 ),
             Actions\CreateAction::make(),
         ];
