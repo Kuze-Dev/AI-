62255d6c4ba114ded8156ca207336ddbb2f0796b
Jade Soriano <jade.halcyondigital@gmail.com>
2023-11-13 16:20:40 +0800

    fix migrations
    Signed-off-by: Jade Soriano <jade.halcyondigital@gmail.com>

---
+++ database/migrations/tenant/settings/2023_11_13_081635_add_fields_to_service_settings.php
@@ -0,0 +1,18 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->add('service.domain_path_segment');
+        $this->migrator->add('service.admin_should_receive', false);
+        $this->migrator->add('service.admin_main_receiver', '');
+        $this->migrator->add('service.admin_cc', null);
+        $this->migrator->add('service.admin_bcc', null);
+        $this->migrator->add('service.email_sender_name', '');
+        $this->migrator->add('service.email_reply_to', null);
+        $this->migrator->add('service.email_footer', null);
+    }
+};
