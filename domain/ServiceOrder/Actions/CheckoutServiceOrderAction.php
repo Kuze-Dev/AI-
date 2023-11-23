222801952558886e97fe86e4d05a62b24d79cfc0
Jerome Hipolito <jeromehipolito.halcyondigital@gmail.com>
2023-11-23 15:30:51 +0800

    format

--- domain/ServiceOrder/Actions/CheckoutServiceOrderAction.php
+++ domain/ServiceOrder/Actions/CheckoutServiceOrderAction.php
@@ -139,7 +139,7 @@ class CheckoutServiceOrderAction
             return $result;
         }
 
-        throw new PaymentException($result->message ?? "Payment Unauthorized");
+        throw new PaymentException($result->message ?? 'Payment Unauthorized');
     }
 
     /** @throws Throwable */
