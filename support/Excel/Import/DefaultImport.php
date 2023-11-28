43ff0b38ae40cbe5320a11eef583a7ad53bed027
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2023-11-29 07:50:47 +0800

    Sort argument
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- support/Excel/Import/DefaultImport.php
+++ support/Excel/Import/DefaultImport.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\SerializableClosure\SerializableClosure;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -15,7 +16,7 @@ use Maatwebsite\Excel\Concerns\WithValidation;
 use Maatwebsite\Excel\Events\ImportFailed;
 use Support\Excel\Listeners\SendImportFailedNotification;
 
-class DefaultImport implements ShouldQueue, ToModel, WithChunkReading, WithEvents, WithHeadingRow, WithValidation
+class DefaultImport implements ShouldQueue, ToModel, WithBatchInserts, WithChunkReading, WithEvents, WithHeadingRow, WithValidation
 {
     public function __construct(
         private readonly Model $user,
@@ -24,6 +25,11 @@ class DefaultImport implements ShouldQueue, ToModel, WithChunkReading, WithEvent
         private readonly array $validateMessages = [],
         private readonly array $validateAttributes = []
     ) {
+    }
+
+    public function batchSize(): int
+    {
+        return 1_000;
     }
 
     public function chunkSize(): int
