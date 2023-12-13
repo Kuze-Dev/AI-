20a81db6a3d8e68aa1538f675f1f12ef3bd1141a
Jerome Hipolito <jeromehipolito.halcyondigital@gmail.com>
2023-12-13 08:07:34 +0800

    remove validate user

--- domain/ServiceOrder/Actions/CheckoutServiceOrderAction.php
+++ domain/ServiceOrder/Actions/CheckoutServiceOrderAction.php
@@ -45,8 +45,6 @@ class CheckoutServiceOrderAction
 
         $this->serviceOrder = $this->prepareServiceOrder();
 
-        $this->validateUser();
-
         return $this->proceedPayment();
     }
 
