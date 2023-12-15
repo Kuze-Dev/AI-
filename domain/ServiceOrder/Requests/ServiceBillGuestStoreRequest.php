d44fb8f91afcb4ce9f5e5cccc92c0cf54014f6f0
Jerome Hipolito <jeromehipolito.halcyondigital@gmail.com>
2023-12-15 14:49:40 +0800

    service bill list error fix

---
+++ domain/ServiceOrder/Requests/ServiceBillGuestStoreRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\ServiceOrder\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ServiceBillGuestStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'reference' => [
+                'string',
+            ],
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation Error',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}
