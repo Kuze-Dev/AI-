2778028aecdbec1efab7f5ea7a68bdebe72c183d
Jerome Hipolito <jeromehipolito.halcyondigital@gmail.com>
2024-01-30 10:33:14 +0800

    fixed rule

--- app/FilamentTenant/Resources/ServiceOrderResource/Rules/PaymentPlanAmountRule.php
+++ app/FilamentTenant/Resources/ServiceOrderResource/Rules/PaymentPlanAmountRule.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Validation\ValidationRule;
 
 class PaymentPlanAmountRule implements ValidationRule
 {
-     public function __construct(
+    public function __construct(
         protected readonly float $totalPrice,
         protected readonly string $payment_value,
     ) {
@@ -20,18 +20,19 @@ class PaymentPlanAmountRule implements ValidationRule
     {
         $amounts = array_column($value, 'amount');
         $sum = array_sum(array_map('floatval', $amounts));
-        
-        if($this->payment_value === PaymentPlanValue::FIXED->value){
-                if($this->totalPrice !== $sum){
-                    $fail('The payment_plan amount must be equal to total price.');
-                }
+
+        if ($this->payment_value === PaymentPlanValue::FIXED->value) {
+            if ($this->totalPrice !== $sum) {
+                $fail('The payment_plan amount must be equal to total price.');
+
+            }
         }
 
-        if($this->payment_value === PaymentPlanValue::PERCENT->value){
-                if($sum !== floatval(100)){
-                    $fail('The payment_plan amount must be equal to 100.');
-                }
+        if ($this->payment_value === PaymentPlanValue::PERCENT->value) {
+            if ($sum !== floatval(100)) {
+                $fail('The payment_plan amount must be equal to 100.');
+            }
         }
-        
+
     }
 }
