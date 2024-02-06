f222096d76ed9a94e3f7dfd8dc27f3ea05da445e
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2024-02-06 15:37:23 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- app/FilamentTenant/Resources/CurrencyResource/Pages/ListCurrency.php
+++ app/FilamentTenant/Resources/CurrencyResource/Pages/ListCurrency.php
@@ -5,17 +5,9 @@ declare(strict_types=1);
 namespace App\FilamentTenant\Resources\CurrencyResource\Pages;
 
 use App\FilamentTenant\Resources\CurrencyResource;
-use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCurrency extends ListRecords
 {
     protected static string $resource = CurrencyResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
