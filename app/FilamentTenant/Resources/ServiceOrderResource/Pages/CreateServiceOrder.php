59b10c3b7f248f7d0e42d63b976ab99eca3ab53e
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2024-02-20 13:28:55 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- app/FilamentTenant/Resources/ServiceOrderResource/Pages/CreateServiceOrder.php
+++ app/FilamentTenant/Resources/ServiceOrderResource/Pages/CreateServiceOrder.php
@@ -8,7 +8,7 @@ use App\Filament\Pages\Concerns\LogsFormActivity;
 use App\FilamentTenant\Resources\ServiceOrderResource;
 use Domain\ServiceOrder\Actions\CreateServiceOrderAction;
 use Domain\ServiceOrder\DataTransferObjects\ServiceOrderData;
-use Filament\Pages\Actions\Action;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
