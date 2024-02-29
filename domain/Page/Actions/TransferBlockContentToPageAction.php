0316ad7b0dfb0dcbb9ea1540f75e8f27fad1389d
Roy Biscocho <rbiscocho.halcyondigital@gmail.com>
2024-02-29 09:44:34 +0800

    fix page draft issue
    Signed-off-by: Roy Biscocho <rbiscocho.halcyondigital@gmail.com>

---
+++ domain/Page/Actions/TransferBlockContentToPageAction.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Page\Actions;
+
+use Domain\Blueprint\Actions\UpdateBlueprintDataAction;
+use Domain\Page\DataTransferObjects\BlockContentData;
+use Domain\Page\Models\BlockContent;
+use Domain\Page\Models\Page;
+
+class TransferBlockContentToPageAction
+{
+    public function __construct(
+        protected UpdateBlueprintDataAction $updateBlueprintDataAction,
+    ) {
+    }
+
+    public function execute(Page $page, BlockContentData $blockContentData): BlockContent
+    {
+        $blockContent = BlockContent::findorfail($blockContentData->id);
+
+        $blockContent->update([
+            'page_id' => $page->id,
+            'block_id' => $blockContentData->block_id,
+            'data' => $blockContentData->data,
+        ]);
+
+        $this->updateBlueprintDataAction->execute($blockContent);
+
+        return $blockContent;
+    }
+}
