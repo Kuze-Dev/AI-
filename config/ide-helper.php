8d5729a7ddb4ee0e3b469433536d7ff5492c65d7
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2024-03-04 07:49:43 +0800

    Upgrade barryvdh/laravel-ide-helper to v3
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- config/ide-helper.php
+++ config/ide-helper.php
@@ -11,7 +11,7 @@ return [
     |
     */
 
-    'filename' => '_ide_helper.php',
+    'filename'  => '_ide_helper.php',
 
     /*
     |--------------------------------------------------------------------------
@@ -122,7 +122,7 @@ return [
     'include_helpers' => false,
 
     'helper_files' => [
-        base_path().'/vendor/laravel/framework/src/Illuminate/Support/helpers.php',
+        base_path() . '/vendor/laravel/framework/src/Illuminate/Support/helpers.php',
     ],
 
     /*
@@ -202,36 +202,6 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Support for custom DB types
-    |--------------------------------------------------------------------------
-    |
-    | This setting allow you to map any custom database type (that you may have
-    | created using CREATE TYPE statement or imported using database plugin
-    | / extension to a Doctrine type.
-    |
-    | Each key in this array is a name of the Doctrine2 DBAL Platform. Currently valid names are:
-    | 'postgresql', 'db2', 'drizzle', 'mysql', 'oracle', 'sqlanywhere', 'sqlite', 'mssql'
-    |
-    | This name is returned by getName() method of the specific Doctrine/DBAL/Platforms/AbstractPlatform descendant
-    |
-    | The value of the array is an array of type mappings. Key is the name of the custom type,
-    | (for example, "jsonb" from Postgres 9.4) and the value is the name of the corresponding Doctrine2 type (in
-    | our case it is 'json_array'. Doctrine types are listed here:
-    | https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/types.html#types
-    |
-    | So to support jsonb in your models when working with Postgres, just add the following entry to the array below:
-    |
-    | "postgresql" => array(
-    |       "jsonb" => "json_array",
-    |  ),
-    |
-    */
-    'custom_db_types' => [
-
-    ],
-
-    /*
      |--------------------------------------------------------------------------
      | Support for camel cased models
      |--------------------------------------------------------------------------
@@ -295,6 +265,17 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Use generics syntax
+    |--------------------------------------------------------------------------
+    |
+    | Use generics syntax within DocBlocks,
+    | e.g. `Collection<User>` instead of `Collection|User[]`.
+    |
+    */
+    'use_generics_annotations' => true,
+
+    /*
+    |--------------------------------------------------------------------------
     | Additional relation types
     |--------------------------------------------------------------------------
     |
@@ -304,6 +285,20 @@ return [
     |
     */
     'additional_relation_types' => [],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Additional relation return types
+    |--------------------------------------------------------------------------
+    |
+    | When using custom relation types its possible for the class name to not contain
+    | the proper return type of the relation. The key of the array is the relationship
+    | method name. The value of the array is the return type of the relation ('many'
+    | or 'morphTo').
+    | e.g. `'relationName' => 'many'`.
+    |
+    */
+    'additional_relation_return_types' => [],
 
     /*
     |--------------------------------------------------------------------------
