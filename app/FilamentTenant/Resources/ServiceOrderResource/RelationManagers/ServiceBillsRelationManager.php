20d0e83c4d6f54de0974cbc26aa341db1d919a43
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2024-03-04 14:24:46 +0800

    fix
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- app/FilamentTenant/Resources/ServiceOrderResource/RelationManagers/ServiceBillsRelationManager.php
+++ app/FilamentTenant/Resources/ServiceOrderResource/RelationManagers/ServiceBillsRelationManager.php
@@ -6,6 +6,7 @@ namespace App\FilamentTenant\Resources\ServiceOrderResource\RelationManagers;
 
 use Domain\Payments\Enums\PaymentRemark;
 use Domain\Payments\Enums\PaymentStatus;
+use Domain\Payments\Models\Payment;
 use Domain\ServiceOrder\Enums\ServiceBillStatus;
 use Domain\ServiceOrder\Events\AdminServiceBillBankPaymentEvent;
 use Domain\ServiceOrder\Models\ServiceBill;
@@ -170,8 +171,10 @@ class ServiceBillsRelationManager extends RelationManager
                     ->icon('heroicon-m-eye')
                     ->color('gray')
                     ->slideOver()
-                    ->visible(fn (ServiceBill $record) => $record->status === ServiceBillStatus::FOR_APPROVAL)
-                    ->form([
+                    ->visible(
+                        fn (ServiceBill $record) => $record->status === ServiceBillStatus::FOR_APPROVAL
+                    )
+                    ->form(fn (ServiceBill $record) => [
                         Forms\Components\Textarea::make('customer_message')
                             ->label(trans('Customer Message'))
                             ->formatStateUsing(function (ServiceBill $record) {
@@ -179,41 +182,28 @@ class ServiceBillsRelationManager extends RelationManager
                             })
                             ->disabled()
                             ->dehydrated(false),
-                        //                                    Forms\Components\FileUpload::make('bank_proof_image')
-                        //                                        ->label(trans('Customer Upload'))
-                        //                                        ->formatStateUsing(function (ServiceBill $record) {
-                        //                                            return $record->latestPayment()?->getMedia('image')
-                        //                                                ->mapWithKeys(fn (Media $file) => [$file->uuid => $file->uuid])
-                        //                                                ->toArray() ?? [];
-                        //                                        })
-                        //                                        ->hidden(function () use ($record) {
-                        //                                            return (bool) (empty($record->latestPayment()?->getFirstMediaUrl('image')));
-                        //                                        })
-                        //                                        ->image()
-                        //                                        ->getUploadedFileUrlUsing(static function (
-                        //                                            Forms\Components\FileUpload $component,
-                        //                                            string $file
-                        //                                        ): ?string {
-                        //                                            $mediaClass = config('media-library.media_model', Media::class);
-                        //
-                        //                                            /** @var ?Media $media */
-                        //                                            $media = $mediaClass::findByUuid($file);
-                        //
-                        //                                            if ($component->getVisibility() === 'private') {
-                        //                                                try {
-                        //                                                    return $media?->getTemporaryUrl(now()->addMinutes(5));
-                        //                                                } catch (Throwable) {
-                        //                                                }
-                        //                                            }
-                        //
-                        //                                            return $media?->getUrl();
-                        //                                        })->disabled(),
+
+                        Forms\Components\SpatieMediaLibraryFileUpload::make('image')
+                            ->label(trans('Customer Upload'))
+                            ->collection('image')
+                            ->model(fn () => $record->latestPayment())
+                            ->visible(
+                                fn (Payment $record) => $record->getFirstMedia('image') !== null
+                            )
+                            ->disabled()
+                            ->formatStateUsing(
+                                fn (Payment $record) => $record->getMedia('image')
+                                    ->mapWithKeys(fn (Media $media) => [$media->uuid => $media->uuid])
+                                    ->toArray()
+                            ),
+
                         Forms\Components\Select::make('payment_remark')
                             ->label('Status')
                             ->required()
                             ->options(PaymentRemark::class)
                             ->enum(PaymentRemark::class)
                             ->required(),
+
                         Forms\Components\Textarea::make('admin_message')
                             ->translateLabel()
                             ->maxLength(255)
