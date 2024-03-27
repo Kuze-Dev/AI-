ab26806dcf7aeacb9b2ceed20b15c78f64aba25a
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2024-03-27 09:08:20 +0800

    product import/export (wip)
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- app/FilamentTenant/Resources/ProductResource/Pages/ListProducts.php
+++ app/FilamentTenant/Resources/ProductResource/Pages/ListProducts.php
@@ -6,19 +6,14 @@ namespace App\FilamentTenant\Resources\ProductResource\Pages;
 
 use App\Features\ECommerce\ProductBatchUpdate;
 use App\FilamentTenant\Resources\ProductResource;
-use Domain\Product\Enums\Decision;
-use Domain\Product\Enums\Status;
 use Domain\Product\Exports\ProductExporter;
 use Domain\Product\Exports\ProductVariantExporter;
 use Domain\Product\Imports\ProductBatchUpdateImporter;
 use Domain\Product\Imports\ProductImporter;
 use Domain\Product\Imports\ProductVariantImporter;
-use Domain\Product\Models\Product;
 use Domain\Tenant\TenantFeatureSupport;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-use HalcyonAgile\FilamentExport\Actions\ExportAction;
-use Illuminate\Database\Eloquent\Builder;
 
 class ListProducts extends ListRecords
 {
@@ -57,69 +52,6 @@ class ListProducts extends ListRecords
                 ->icon('heroicon-o-arrow-down-tray')
                 ->color('primary'),
 
-            Actions\CreateAction::make(),
-        ];
-
-        return [
-            ExportAction::make()
-                ->model(Product::class)
-                ->queue()
-                ->query(
-                    fn (Builder $query) => $query->with('productVariants')->latest()
-                )
-                ->mapUsing(
-                    [
-                        'product_id', 'is_variant', 'variant_id', 'name', 'variant_combination', 'sku',
-                        'retail_price', 'selling_price', 'stock', 'status', 'is_digital_product',
-                        'is_featured', 'is_special_offer', 'allow_customer_remarks', 'allow_stocks',
-                        'allow_guest_purchase', 'weight', 'length', 'width', 'height', 'minimum_order_quantity',
-                    ],
-                    function (Product $product) {
-                        $productData = [
-                            [
-                                $product->id,
-                                Decision::NO->value,
-                                '',
-                                $product->name,
-                                '',
-                                $product->sku,
-                                $product->retail_price,
-                                $product->selling_price,
-                                $product->stock,
-                                $product->status ? Status::ACTIVE->value : STATUS::INACTIVE->value,
-                                $product->is_digital_product ? Decision::YES->value : Decision::NO->value,
-                                $product->is_featured ? Decision::YES->value : Decision::NO->value,
-                                $product->is_special_offer ? Decision::YES->value : Decision::NO->value,
-                                $product->allow_customer_remarks ? Decision::YES->value : Decision::NO->value,
-                                $product->allow_stocks ? Decision::YES->value : Decision::NO->value,
-                                $product->allow_guest_purchase ? Decision::YES->value : Decision::NO->value,
-                                $product->weight,
-                                $product->dimension['length'] ?? '',
-                                $product->dimension['width'] ?? '',
-                                $product->dimension['height'] ?? '',
-                                $product->minimum_order_quantity,
-                            ],
-                        ];
-                        foreach ($product->productVariants as $variant) {
-                            $productData[] =
-                                [
-                                    $variant->product_id,
-                                    Decision::YES->value,
-                                    $variant->id,
-                                    '',
-                                    $variant->combination,
-                                    $variant->sku,
-                                    $variant->retail_price,
-                                    $variant->selling_price,
-                                    $variant->stock,
-                                    $variant->status ? Status::ACTIVE->value : STATUS::INACTIVE->value,
-
-                                ];
-                        }
-
-                        return $productData;
-                    }
-                ),
             Actions\CreateAction::make(),
         ];
     }
