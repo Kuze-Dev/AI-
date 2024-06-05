9ecbfcb03c06d44196554fc8552f20c9cb529e83
Roy Biscocho <rbiscocho.halcyondigital@gmail.com>
2024-06-05 14:38:30 +0800

    temporary comment sanitizeData
    Signed-off-by: Roy Biscocho <rbiscocho.halcyondigital@gmail.com>

--- domain/Content/Actions/UpdateContentEntryAction.php
+++ domain/Content/Actions/UpdateContentEntryAction.php
@@ -28,18 +28,18 @@ class UpdateContentEntryAction
      */
     public function execute(ContentEntry $contentEntry, ContentEntryData $contentEntryData): ContentEntry
     {
-        $sanitizeData = array_merge($contentEntryData->data,
-            $this->sanitizeBlueprintData(
-                $contentEntryData->data,
-                $contentEntry->content->blueprint->schema->getFieldStatekeys(),
-            )
-        );
+        // $sanitizeData = array_merge($contentEntryData->data,
+        //     $this->sanitizeBlueprintData(
+        //         $contentEntryData->data,
+        //         $contentEntry->content->blueprint->schema->getFieldStatekeys(),
+        //     )
+        // );
 
         $contentEntry->update([
             'author_id' => $contentEntryData->author_id,
             'title' => $contentEntryData->title,
             'published_at' => $contentEntryData->published_at,
-            'data' => $sanitizeData,
+            'data' => $contentEntryData->data,
             'locale' => $contentEntryData->locale ?? Locale::where('is_default', true)->first()?->code,
         ]);
 
