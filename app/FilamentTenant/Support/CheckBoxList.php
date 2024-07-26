ea8b6d1b37fcf5abb2fd5cd9767955c2ad2254f5
Roy Biscocho <rbiscocho.halcyondigital@gmail.com>
2024-07-26 15:18:06 +0800

    extend checkboxlist functionality add candisabledoption
    Signed-off-by: Roy Biscocho <rbiscocho.halcyondigital@gmail.com>

---
+++ app/FilamentTenant/Support/CheckBoxList.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FilamentTenant\Support;
+
+use Filament\Forms\Components\CheckboxList as ComponentsCheckboxList;
+use Filament\Forms\Components\Concerns\CanDisableOptions;
+
+class CheckBoxList extends ComponentsCheckboxList
+{
+    use CanDisableOptions;
+
+    protected string $view = 'filament.forms.components.checkbox-list';
+}
