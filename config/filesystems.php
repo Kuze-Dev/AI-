df905f11d8854675525c40ab32fe488f2dd8adc9
Lloric Mayuga Garcia <lloricode@gmail.com>
2025-01-15 13:58:05 +0800

    Update configs from laravel/laravel
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- config/filesystems.php
+++ config/filesystems.php
@@ -9,7 +9,7 @@ return [
     |
     | Here you may specify the default filesystem disk that should be used
     | by the framework. The "local" disk, as well as a variety of cloud
-    | based disks are available to your application. Just store away!
+    | based disks are available to your application for file storage.
     |
     */
 
@@ -20,11 +20,11 @@ return [
     | Filesystem Disks
     |--------------------------------------------------------------------------
     |
-    | Here you may configure as many filesystem "disks" as you wish, and you
-    | may even configure multiple disks of the same driver. Defaults have
-    | been set up for each driver as an example of the required values.
+    | Below you may configure as many filesystem disks as necessary, and you
+    | may even configure multiple disks for the same driver. Examples for
+    | most supported storage drivers are configured here for reference.
     |
-    | Supported Drivers: "local", "ftp", "sftp", "s3"
+    | Supported drivers: "local", "ftp", "sftp", "s3"
     |
     */
 
@@ -32,14 +32,15 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('app/private'),
+            'serve' => true,
             'throw' => false,
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -53,24 +54,8 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => true,
-            'options' => [
-                'CacheControl' => 'max-age=315360000'
-            ]
-        ],
-
-        'r2' => [
-            'driver' => 's3',
-            'key' => null, //env('CLOUDFLARE_R2_ACCESS_KEY_ID'),
-            'secret' => null, //env('CLOUDFLARE_R2_SECRET_ACCESS_KEY'),
-            'region' => 'us-east-1', // Cloudflare R2 doesn't have specific regions, so 'us-east-1' is fine.
-            'bucket' => null, //env('CLOUDFLARE_R2_BUCKET'),
-            'url' => null, //env('CLOUDFLARE_R2_URL'),
-            'visibility' => 'private',
-            'endpoint' => null, //env('CLOUDFLARE_R2_ENDPOINT'),
-            'use_path_style_endpoint' => null, //env('CLOUDFLARE_R2_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
-       ],
+        ],
 
     ],
 
