24b3eb5557283bd6096f0cd0980fae79ffbcbf53
Lloric Mayuga Garcia <lloricode@gmail.com>
2025-02-20 16:09:33 +0800

    use contextual attributes
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- app/Filament/Requests/VerifyEmailRequest.php
+++ app/Filament/Requests/VerifyEmailRequest.php
@@ -4,21 +4,24 @@ declare(strict_types=1);
 
 namespace App\Filament\Requests;
 
+use App\Attributes\CurrentApiCustomer;
+use Domain\Customer\Models\Customer;
+use Illuminate\Container\Attributes\RouteParameter;
 use Illuminate\Foundation\Http\FormRequest;
 
-/**
- * @property-read string|null $id
- * @property-read string|null $hash
- */
 class VerifyEmailRequest extends FormRequest
 {
-    public function authorize(): bool
+    public function authorize(
+        #[RouteParameter('id')] string|null $id,
+        #[RouteParameter('hash')] string|null $hash,
+        #[CurrentApiCustomer] Customer $customer,
+    ) : bool
     {
-        if (! hash_equals($this->id ?? '', (string) $this->user()?->getKey())) {
+        if (! hash_equals($id ?? '' , (string) $customer->getKey())) {
             return false;
         }
 
-        return hash_equals($this->hash ?? '', sha1((string) $this->user()?->getEmailForVerification()));
+        return hash_equals($hash ?? '', sha1($customer->getEmailForVerification()));
     }
 
     public function rules(): array
