d893aecb56ea5f813eaf161bb0c1ba49dfeda6b3
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2025-03-03 08:11:01 +0800

    reuse
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- app/helpers.php
+++ app/helpers.php
@@ -6,8 +6,6 @@ use Domain\Admin\Models\Admin;
 use Domain\Customer\Models\Customer;
 use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Str;
 
 if (! function_exists('filament_admin')) {
 
@@ -22,14 +20,13 @@ if(! function_exists('guest_customer_logged_in')) {
     function guest_customer_logged_in(): ?Customer
     {
         /** @phpstan-ignore return.type */
-        return once(fn () => auth('api')->user());
+        return once(fn () => Auth::guard('api')->user());
     }
 }
 
 if(! function_exists('customer_logged_in')) {
     function customer_logged_in(): Customer
     {
-        /** @phpstan-ignore return.type */
-        return once(fn () => auth('api')->user());
+        return guest_customer_logged_in();
     }
 }
