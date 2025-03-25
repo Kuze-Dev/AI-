45673bcf6418163e7476897d6b5a26706ee1ad2e
Roy Biscocho <roybisco@publicisgroupe.net>
2025-03-25 09:32:20 +0800

    wip
    Signed-off-by: Roy Biscocho <roybisco@publicisgroupe.net>

--- app/Http/Middleware/RestrictApiAccess.php
+++ app/Http/Middleware/RestrictApiAccess.php
@@ -17,29 +17,54 @@ class RestrictApiAccess
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $origin = $request->header('Origin');
-
-        $allowed_origins = config('cors.allowed_origins');
-
-        if (in_array('*', $allowed_origins, true)) {
+        if ($this->isAllowedOrigin($request) || $this->hasValidRateKey($request)) {
             return $next($request);
         }
 
-        if ($request->hasHeader('x-rate-key')) {
-
-            $ratekey = $request->header('x-rate-key');
-
-            if ($ratekey === config('custom.rate_limit_key')) {
-
-                return $next($request);
-            }
-        }
-
-        // If the request is an API request but the origin is not allowed, deny access
-        if ($request->is('api/*') && (! in_array($origin, config('cors.allowed_origins'), true))) {
-            return response()->json(['message' => 'Access denied '], 403);
+        if ($request->is('api/*')) {
+            return response()->json(['message' => 'Access denied'], 403);
         }
 
         return $next($request);
+    }
+
+    private function isAllowedOrigin(Request $request): bool
+    {
+        $origin = $request->header('Origin');
+        $allowedOrigins = config('cors.allowed_origins');
+
+        if (in_array('*', $allowedOrigins, true)) {
+            return true;
+        }
+
+        foreach ($allowedOrigins as $allowedOrigin) {
+            if ($origin === $allowedOrigin || $this->matchesWildcardOrigin($origin, $allowedOrigin)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function matchesWildcardOrigin(?string $origin, string $allowedOrigin): bool
+    {
+        if (str_starts_with($allowedOrigin, '*.') && $origin) {
+            $allowedBaseDomain = substr($allowedOrigin, 2);
+
+            return str_ends_with($origin, ".$allowedBaseDomain");
+        }
+
+        return false;
+    }
+
+    private function hasValidRateKey(Request $request): bool
+    {
+        if ($request->hasHeader('x-rate-key')) {
+            $rateKey = $request->header('x-rate-key');
+
+            return $rateKey === config('custom.rate_limit_key');
+        }
+
+        return false;
     }
 }
