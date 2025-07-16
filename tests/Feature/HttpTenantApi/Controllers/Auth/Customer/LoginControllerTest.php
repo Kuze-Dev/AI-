cc7e41adb8c055e09df41e34c8c98af4302d4acf
Roy Biscocho <roybisco@publicisgroupe.net>
2025-07-16 10:22:30 +0800

    update test
    Signed-off-by: Roy Biscocho <roybisco@publicisgroupe.net>

--- tests/Feature/HttpTenantApi/Controllers/Auth/Customer/LoginControllerTest.php
+++ tests/Feature/HttpTenantApi/Controllers/Auth/Customer/LoginControllerTest.php
@@ -96,7 +96,7 @@ it('can not access private route with invalid token', function (?string $token) 
     getJson('api/test-private-route', [
         'Authorization' => $token,
     ])
-        ->assertUnauthorized();
+        ->assertStatus(403);
 })
     ->with([
         null,
@@ -112,5 +112,5 @@ it('can not access private route without header authorization', function () {
         ]);
 
     getJson('api/test-private-route')
-        ->assertUnauthorized();
+        ->assertStatus(403);
 });
