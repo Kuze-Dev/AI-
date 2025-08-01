82d3a6aa47a831dd2e90093656790b609316eb08
Roy Biscocho <roybisco@publicisgroupe.net>
2025-08-01 13:16:58 +0800

    wip
    Signed-off-by: Roy Biscocho <roybisco@publicisgroupe.net>

--- app/Tenancy/Bootstrappers/SpatiePermissionBootstrapper.php
+++ app/Tenancy/Bootstrappers/SpatiePermissionBootstrapper.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tenancy\Bootstrappers;
 
+use Illuminate\Contracts\Foundation\Application;
 use Spatie\Permission\PermissionRegistrar;
 use Stancl\Tenancy\Contracts\TenancyBootstrapper;
 use Stancl\Tenancy\Contracts\Tenant;
@@ -12,15 +13,21 @@ class SpatiePermissionBootstrapper implements TenancyBootstrapper
 {
     public function __construct(
         protected PermissionRegistrar $registrar,
+        protected Application $app
     ) {}
 
     public function bootstrap(Tenant $tenant): void
     {
         $this->registrar->cacheKey = 'spatie.permission.cache.tenant.'.$tenant->getTenantKey();
+
+        $this->app->make('config')->set('permission.cache.key', 'tenant.'.$tenant->getTenantKey());
+
     }
 
     public function revert(): void
     {
         $this->registrar->cacheKey = 'spatie.permission.cache';
+
+        $this->app->make('config')->set('permission.cache.key', 'spatie.permission.cache');
     }
 }
